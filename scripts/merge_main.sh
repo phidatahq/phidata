a453d2d0a1be16951757e18bfef8c8f08c36ad6b
Ashpreet Bedi <ashpreet@phidata.com>
2022-05-04 21:39:54 -0400

    Auto commit at Wed May  4 21:39:54 EDT 2022

--- scripts/merge_main.sh
+++ scripts/merge_main.sh
@@ -4,18 +4,18 @@
 #
 # Script to merge/push existing branch to the mainbranch:
 # Usage:
-#   ./scripts/merge.sh
+#   ./scripts/merge_main.sh
 #     - commit current branch
 #     - merge to `main` branch
 #     - push to remote origin `main` branch
 #
-#   ./scripts/merge.sh y
+#   ./scripts/merge_main.sh y
 #     - Default yes to user validation
 #
 ############################################################################
 
 SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-PHILAB_DIR="$( dirname ${SCRIPTS_DIR} )"
+REPO_ROOT="$( dirname ${SCRIPTS_DIR} )"
 source ${SCRIPTS_DIR}/_utils.sh
 
 main() {
@@ -24,27 +24,27 @@ main() {
     YES=1
   fi
 
-  GIT_BRANCH=`git -C ${PHILAB_DIR} rev-parse --abbrev-ref HEAD`
-  GIT_COMMIT=`git -C ${PHILAB_DIR} rev-parse HEAD`
+  GIT_BRANCH=`git -C ${REPO_ROOT} rev-parse --abbrev-ref HEAD`
+  GIT_COMMIT=`git -C ${REPO_ROOT} rev-parse HEAD`
 
   print_horizontal_line
-  print_info_2 "PHILAB_DIR       : ${PHILAB_DIR}"
+  print_info_2 "REPO_ROOT        : ${REPO_ROOT}"
   print_info_2 "GIT_BRANCH       : ${GIT_BRANCH}"
   print_info_2 "GIT_COMMIT       : ${GIT_COMMIT}"
   print_info_2 "DATETIME         : $(date)"
   print_horizontal_line
 
-  print_info_2 "Auto commiting all files in ${PHILAB_DIR}"
+  print_info_2 "Auto commiting all files in ${REPO_ROOT}"
   if [[ ${YES} -ne 1 ]]; then
     space_to_continue
   fi
-  git -C ${PHILAB_DIR} add .
-  git -C ${PHILAB_DIR} commit -am "Auto commit at $(date)"
+  git -C ${REPO_ROOT} add .
+  git -C ${REPO_ROOT} commit -am "Auto commit at $(date)"
 
   print_info "Merging ${GIT_BRANCH} to main branch"
-  git -C ${PHILAB_DIR} checkout main
-  git -C ${PHILAB_DIR} merge --no-ff ${GIT_BRANCH}
-  git -C ${PHILAB_DIR} push -f origin main
+  git -C ${REPO_ROOT} checkout main
+  git -C ${REPO_ROOT} merge --no-ff ${GIT_BRANCH}
+  git -C ${REPO_ROOT} push -f origin main
   print_info "philab pushed to main"
 }
 

d967c41dabd37051048a200d46ee0af85236338a
Ashpreet Bedi <ashpreet@phidata.com>
2023-02-07 11:46:54 -0500

    spark app

---
+++ phidata/app/spark/__init__.py
@@ -0,0 +1,8 @@
+from phidata.app.spark.spark_driver import (
+    SparkDriver,
+    SparkBase,
+    SparkBaseArgs,
+    RestartPolicy,
+    ImagePullPolicy,
+)
+from phidata.app.spark.spark_worker import SparkWorker

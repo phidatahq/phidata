ded60318913acdaa22b40bdd945491f6bae9aa20
Ashpreet Bedi <ashpreet@phidata.com>
2023-04-06 14:17:47 -0400

    lb create

--- phidata/aws/resource/elb/load_balancer.py
+++ phidata/aws/resource/elb/load_balancer.py
@@ -16,26 +16,14 @@ class LoadBalancer(AwsResource):
 
     # Name of the cluster.
     name: str
-    # Name for the cluster.
-    # Use name if not provided.
-    ecs_cluster_name: Optional[str] = None
-
+    subnets: Optional[List[str]] = None
+    subnet_mappings: Optional[List[Dict[str, str]]] = None
+    security_groups: Optional[List[str]] = None
+    scheme: Optional[str] = None
     tags: Optional[List[Dict[str, str]]] = None
-    # The setting to use when creating a cluster.
-    settings: Optional[List[Dict[str, Any]]] = None
-    # The execute command configuration for the cluster.
-    configuration: Optional[Dict[str, Any]] = None
-    # The short name of one or more capacity providers to associate with the cluster.
-    # A capacity provider must be associated with a cluster before it can be included as part of the default capacity
-    # provider strategy of the cluster or used in a capacity provider strategy when calling the CreateService/RunTask.
-    capacity_providers: Optional[List[str]] = None
-    # The capacity provider strategy to set as the default for the cluster. After a default capacity provider strategy
-    # is set for a cluster, when you call the RunTask or CreateService APIs with no capacity provider strategy or
-    # launch type specified, the default capacity provider strategy for the cluster is used.
-    default_capacity_provider_strategy: Optional[List[Dict[str, Any]]] = None
-
-    def get_ecs_cluster_name(self):
-        return self.ecs_cluster_name or self.name
+    type: Optional[str] = None
+    ip_address_type: Optional[str] = None
+    customer_owned_ipv_4_pool: Optional[str] = None
 
     def _create(self, aws_client: AwsApiClient) -> bool:
         """Creates the LoadBalancer
@@ -47,28 +35,32 @@ class LoadBalancer(AwsResource):
 
         # create a dict of args which are not null, otherwise aws type validation fails
         not_null_args: Dict[str, Any] = {}
+        if self.subnets is not None:
+            not_null_args["Subnets"] = self.subnets
+        if self.subnet_mappings is not None:
+            not_null_args["SubnetMappings"] = self.subnet_mappings
+        if self.security_groups is not None:
+            not_null_args["SecurityGroups"] = self.security_groups
+        if self.scheme is not None:
+            not_null_args["Scheme"] = self.scheme
         if self.tags is not None:
             not_null_args["tags"] = self.tags
-        if self.settings is not None:
-            not_null_args["settings"] = self.settings
-        if self.configuration is not None:
-            not_null_args["configuration"] = self.configuration
-        if self.capacity_providers is not None:
-            not_null_args["capacityProviders"] = self.capacity_providers
-        if self.default_capacity_provider_strategy is not None:
-            not_null_args[
-                "defaultCapacityProviderStrategy"
-            ] = self.default_capacity_provider_strategy
+        if self.type is not None:
+            not_null_args["Type"] = self.type
+        if self.ip_address_type is not None:
+            not_null_args["IpAddressType"] = self.ip_address_type
+        if self.customer_owned_ipv_4_pool is not None:
+            not_null_args["CustomerOwnedIpv4Pool"] = self.customer_owned_ipv_4_pool
 
         # Create LoadBalancer
         service_client = self.get_service_client(aws_client)
         try:
-            create_response = service_client.create_cluster(
-                clusterName=self.get_ecs_cluster_name(),
+            create_response = service_client.create_load_balancer(
+                Name=self.name,
                 **not_null_args,
             )
-            logger.debug(f"LoadBalancer: {create_response}")
-            resource_dict = create_response.get("cluster", {})
+            logger.debug(f"Response: {create_response}")
+            resource_dict = create_response.get("LoadBalancers", {})
 
             # Validate resource creation
             if resource_dict is not None:

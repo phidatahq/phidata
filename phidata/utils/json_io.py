63e1226e95774e5c90bbc53f3381acb6af71db4a
Ashpreet Bedi <ashpreet@phidata.com>
2023-06-11 22:27:47 +0100

    update

--- phidata/utils/json_io.py
+++ phidata/utils/json_io.py
@@ -16,14 +16,12 @@ class CustomJSONEncoder(json.JSONEncoder):
 
 def read_json_file(file_path: Optional[Path]) -> Optional[Dict[str, Any]]:
     if file_path is not None and file_path.exists() and file_path.is_file():
-        # logger.debug(f"Reading {file_path}")
+        logger.debug(f"Reading {file_path}")
         return json.loads(file_path.read_text())
     return None
 
 
 def write_json_file(file_path: Optional[Path], data: Optional[Dict[str, Any]]) -> None:
     if file_path is not None and data is not None:
-        import json
-
-        # logger.debug(f"Writing {file_path}")
+        logger.debug(f"Writing {file_path}")
         file_path.write_text(json.dumps(data, cls=CustomJSONEncoder, indent=4))

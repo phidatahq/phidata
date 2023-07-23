8ad16af1324cc1e23dfa0c599e29654a5eae20c7
Ashpreet Bedi <ashpreet@phidata.com>
2023-07-23 13:41:51 +0100

    add conversation schema

--- phi/infra/app/db_app.py
+++ phi/infra/app/db_app.py
@@ -22,16 +22,31 @@ class DbApp(InfraApp):
         return self.db_driver or self.get_secret_from_file("DB_DRIVER")
 
     def get_db_host(self) -> Optional[str]:
-        return "localhost"
+        raise NotImplementedError
 
     def get_db_port(self) -> Optional[int]:
-        return self.host_port
+        raise NotImplementedError
 
-    def get_db_connection_url(self) -> Optional[str]:
+    def get_db_connection(self) -> Optional[str]:
         user = self.get_db_user()
         password = self.get_db_password()
         schema = self.get_db_schema()
         driver = self.get_db_driver()
         host = self.get_db_host()
         port = self.get_db_port()
+        return f"{driver}://{user}:{password}@{host}:{port}/{schema}"
+
+    def get_db_host_local(self) -> Optional[str]:
+        return "localhost"
+
+    def get_db_port_local(self) -> Optional[int]:
+        return self.host_port
+
+    def get_db_connection_local(self) -> Optional[str]:
+        user = self.get_db_user()
+        password = self.get_db_password()
+        schema = self.get_db_schema()
+        driver = self.get_db_driver()
+        host = self.get_db_host_local()
+        port = self.get_db_port_local()
         return f"{driver}://{user}:{password}@{host}:{port}/{schema}"

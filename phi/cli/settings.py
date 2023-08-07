507504f43ea87757fdd14800aa116db5bc46b6e7
Ashpreet Bedi <ashpreet@phidata.com>
2023-08-07 17:46:59 +0100

    v2.0.0.dev18

--- phi/cli/settings.py
+++ phi/cli/settings.py
@@ -12,41 +12,48 @@ class PhiCliSettings(BaseSettings):
     app_name: str = "phi"
     app_version: str = metadata.version("phidata")
 
-    config_file_path: Path = PHI_CLI_DIR.joinpath("config")
     auth_token_path: Path = PHI_CLI_DIR.joinpath("token")
+    config_file_path: Path = PHI_CLI_DIR.joinpath("config")
     credentials_path: Path = PHI_CLI_DIR.joinpath("credentials")
-
-    # These fields are used to update the final signin_url and api_url values
-    # Not to be used directly
-    dev_signin_url: str = "http://localhost:3000/signin"
-    dev_api_url: str = "http://localhost:8000"
-    prd_signin_url: str = "https://phidata.com/signin"
-    prd_api_url: str = "https://api.phidata.com"
-
-    api_runtime: str = "prd"
     auth_token_cookie: str = "__phi_session"
     auth_token_header: str = "X-PHIDATA-AUTH-TOKEN"
-    signin_url: str = Field("https://phidata.com/signin", validate_default=True)
-    api_url: str = Field("https://api.phidata.com", validate_default=True)
 
-    # Feature Gates
+    api_runtime: str = "prd"
     api_enabled: bool = False
+    api_url: str = Field("https://api.phidata.com", validate_default=True)
+    signin_url: str = Field("https://phidata.com/signin", validate_default=True)
 
     model_config = SettingsConfigDict(env_prefix="PHI_")
 
+    @field_validator("api_runtime", mode="before")
+    def validate_runtime_env(cls, v):
+        """Validate api_runtime."""
+
+        valid_api_runtimes = ["dev", "stg", "prd"]
+        if v not in valid_api_runtimes:
+            raise ValueError(f"Invalid api_runtime: {v}")
+
+        return v
+
     @field_validator("signin_url", mode="before")
     def update_signin_url(cls, v, info: FieldValidationInfo):
-        dev_signin_url = info.data.get("dev_signin_url", "http://localhost:3000/signin")
-        prd_signin_url = info.data.get("prd_signin_url", "https://phidata.com/signin")
-        signin_url = dev_signin_url if info.data["api_runtime"] == "dev" else prd_signin_url
-        return signin_url
+        api_runtime = info.data["api_runtime"]
+        if api_runtime == "dev":
+            return "http://localhost:3000/signin"
+        elif api_runtime == "stg":
+            return "https://stgphi.com/signin"
+        else:
+            return "https://phidata.com/signin"
 
     @field_validator("api_url", mode="before")
     def update_api_url(cls, v, info: FieldValidationInfo):
-        dev_api_url = info.data.get("dev_api_url", "http://localhost:8000")
-        prd_api_url = info.data.get("prd_api_url", "https://api.phidata.com")
-        api_url = dev_api_url if info.data["api_runtime"] == "dev" else prd_api_url
-        return api_url
+        api_runtime = info.data["api_runtime"]
+        if api_runtime == "dev":
+            return "http://localhost:3000/signin"
+        elif api_runtime == "stg":
+            return "https://stgphi.com/signin"
+        else:
+            return "https://phidata.com/signin"
 
 
 phi_cli_settings = PhiCliSettings()

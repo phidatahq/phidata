7e5da80a668ab400c4c39b0a1b0385ee6a672ecd
Ashpreet Bedi <ashpreet@phidata.com>
2024-02-04 16:24:57 +0000

    v2.3.15

--- cookbook/ollama/image.py
+++ cookbook/ollama/image.py
@@ -2,9 +2,7 @@ from pathlib import Path
 from phi.assistant import Assistant
 from phi.llm.ollama import Ollama
 
-assistant = Assistant(
-    llm=Ollama(model="llava")
-)
+assistant = Assistant(llm=Ollama(model="llava"))
 
 image_path = Path(__file__).parent / "test_image.jpeg"
 assistant.print_response(

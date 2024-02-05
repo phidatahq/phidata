3a4579697c9bb6acf8d6409c09fae101a4a64cac
Ashpreet Bedi <ashpreet@phidata.com>
2024-02-05 20:24:36 +0000

    DDG Cookbook

--- cookbook/tools/duckduckgo.py
+++ cookbook/tools/duckduckgo.py
@@ -2,4 +2,4 @@ from phi.assistant import Assistant
 from phi.tools.duckduckgo import DuckDuckGo
 
 assistant = Assistant(tools=[DuckDuckGo()], show_tool_calls=True)
-assistant.print_response("Whats happening in France?")
+assistant.print_response("Whats happening in France? Summarize top stories with sources.")

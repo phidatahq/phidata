844149350fffda17e11d522c18cd835bbd282339
ivanfioravanti <ivan.fioravanti@gmail.com>
2024-02-06 01:19:52 +0100

    prompt instead of question

--- cookbook/ollama/who_are_you.py
+++ cookbook/ollama/who_are_you.py
@@ -2,9 +2,9 @@ from phi.assistant import Assistant
 from phi.llm.ollama import Ollama
 
 temp = 0.3
-question = "Who are you? Answer in 1 sentence."
+prompt = "Who are you? Answer in 1 sentence."
 models = ["phi", "llava", "llama2", "mixtral", "openhermes", "tinyllama"]
 
 for model in models:
     print(f"================ {model} ================")
-    Assistant(llm=Ollama(model=model), options={'temperature': temp}).print_response(question)
+    Assistant(llm=Ollama(model=model), options={'temperature': temp}).print_response(prompt)

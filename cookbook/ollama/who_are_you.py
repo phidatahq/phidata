a8d060a1d3a2b553ce6a48b42250ed707e8bc216
ivanfioravanti <ivan.fioravanti@gmail.com>
2024-02-06 01:12:28 +0100

    Refactor `who_are_you.py` to simplify code and adding temperature.

--- cookbook/ollama/who_are_you.py
+++ cookbook/ollama/who_are_you.py
@@ -1,22 +1,13 @@
 from phi.assistant import Assistant
 from phi.llm.ollama import Ollama
 
-prompt = "Who are you and who created you? Answer in 1 short sentence."
+from phi.assistant import Assistant
+from phi.llm.ollama import Ollama
 
-print("================ phi ================")
-Assistant(llm=Ollama(model="phi"), system_prompt=prompt).print_response()
+temp = 0.3
+question = "Who are you? Answer in 1 sentence."
+models = ["phi", "llava", "llama2", "mixtral", "openhermes", "tinyllama"]
 
-print("================ llava ================")
-Assistant(llm=Ollama(model="llava"), system_prompt=prompt).print_response()
-
-print("================ llama2 ================")
-Assistant(llm=Ollama(model="llama2"), system_prompt=prompt).print_response()
-
-print("================ mixtral ================")
-Assistant(llm=Ollama(model="mixtral"), system_prompt=prompt).print_response()
-
-print("================ openhermes ================")
-Assistant(llm=Ollama(model="openhermes"), system_prompt=prompt).print_response()
-
-print("================ tinyllama ================")
-Assistant(llm=Ollama(model="tinyllama"), system_prompt=prompt).print_response()
+for model in models:
+    print(f"================ {model} ================")
+    Assistant(llm=Ollama(model=model), options={'temperature': temp}).print_response(question)

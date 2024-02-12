bed4239e5d44eae6f63d9b376181b91a59b999ba
Ashpreet Bedi <ashpreet@phidata.com>
2024-02-12 16:09:48 +0000

    cookbook update

--- cookbook/ollama/assistant.py
+++ cookbook/ollama/assistant.py
@@ -4,6 +4,5 @@ from phi.llm.ollama import Ollama
 assistant = Assistant(
     llm=Ollama(),
     description="You help people with their health and fitness goals.",
-    debug_mode=True,
 )
 assistant.print_response("Share a quick healthy breakfast recipe.", markdown=True)

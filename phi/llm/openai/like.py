a266af8c70c6bba9aa1546bc258a2ed890e78e30
Ashpreet Bedi <ashpreet@phidata.com>
2024-02-14 23:59:54 +0000

    LM studio cookbook

--- phi/llm/openai/like.py
+++ phi/llm/openai/like.py
@@ -4,4 +4,5 @@ from phi.llm.openai.chat import OpenAIChat
 class OpenAILike(OpenAIChat):
     name: str = "OpenAILike"
     model: str = "not-provided"
+    api_key: str = "not-provided"
     phi_proxy: bool = False

d5220c9def8355c3f541e82f3c1b3dba4f57f2d9
Ashpreet Bedi <ashpreet@phidata.com>
2024-02-15 00:00:50 +0000

    v2.3.39

--- phi/llm/openai/like.py
+++ phi/llm/openai/like.py
@@ -1,8 +1,9 @@
+from typing import Optional
 from phi.llm.openai.chat import OpenAIChat
 
 
 class OpenAILike(OpenAIChat):
     name: str = "OpenAILike"
     model: str = "not-provided"
-    api_key: str = "not-provided"
+    api_key: Optional[str] = "not-provided"
     phi_proxy: bool = False

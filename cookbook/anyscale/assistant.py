a135e0521b71a4cf5edcc52c84dc587fe2f00769
Ashpreet Bedi <ashpreet@phidata.com>
2024-02-16 10:37:53 +0000

    v2.3.39

--- cookbook/anyscale/assistant.py
+++ cookbook/anyscale/assistant.py
@@ -2,7 +2,7 @@ from phi.assistant import Assistant
 from phi.llm.anyscale import Anyscale
 
 assistant = Assistant(
-    llm=Anyscale(),
+    llm=Anyscale(model="mistralai/Mixtral-8x7B-Instruct-v0.1"),
     description="You help people with their health and fitness goals.",
 )
 assistant.print_response("Share a 2 sentence quick healthy breakfast recipe.", markdown=True)

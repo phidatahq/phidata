e13975720f0723dc1a3250080f1cd5f9f5c755c2
Ashpreet Bedi <ashpreet@phidata.com>
2024-02-20 12:55:07 +0000

    formatting

--- phi/k8s/app/__init__.py
+++ phi/k8s/app/__init__.py
@@ -1,1 +1,11 @@
-from phi.k8s.app.base import K8sApp, K8sBuildContext, ContainerContext, RestartPolicy, ImagePullPolicy, ServiceType, K8sWorkspaceVolumeType, AppVolumeType, LoadBalancerProvider  # noqa: F401
+from phi.k8s.app.base import (
+    K8sApp,
+    K8sBuildContext,
+    ContainerContext,
+    RestartPolicy,
+    ImagePullPolicy,
+    ServiceType,
+    K8sWorkspaceVolumeType,
+    AppVolumeType,
+    LoadBalancerProvider,
+)  # noqa: F401

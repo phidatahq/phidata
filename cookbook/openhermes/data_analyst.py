7b5c84f55866d04d639f056580e957fcde5faa06
Ashpreet Bedi <ashpreet@phidata.com>
2024-02-22 15:11:22 +0000

    custom assistants

--- cookbook/openhermes/data_analyst.py
+++ cookbook/openhermes/data_analyst.py
@@ -1,22 +1,21 @@
-import json
+from phi.assistant import Assistant
 from phi.llm.ollama import Ollama
-from phi.assistant.duckdb import DuckDbAssistant
+from phi.tools.duckdb import DuckDbTools
 
-duckdb_assistant = DuckDbAssistant(
-    llm=Ollama(model="openhermes"),
-    show_tool_calls=True,
-    semantic_model=json.dumps(
-        {
-            "tables": [
-                {
-                    "name": "movies",
-                    "description": "Contains information about movies from IMDB.",
-                    "path": "https://phidata-public.s3.amazonaws.com/demo_data/IMDB-Movie-Data.csv",
-                }
-            ]
-        }
-    ),
-    debug_mode=True,
+
+duckdb_tools = DuckDbTools(create_tables=False, export_tables=False, summarize_tables=False)
+duckdb_tools.create_table_from_path(
+    path="https://phidata-public.s3.amazonaws.com/demo_data/IMDB-Movie-Data.csv", table="movies"
 )
 
-duckdb_assistant.print_response("What is the average rating of movies? Show me the SQL.", markdown=True)
+assistant = Assistant(
+    llm=Ollama(model="openhermes"),
+    tools=[duckdb_tools],
+    show_tool_calls=True,
+    add_to_system_prompt="""
+    Here are the tables you have access to:
+    - movies: Contains information about movies from IMDB.
+    """,
+    debug_mode=True,
+)
+assistant.print_response("What is the average rating of movies?", markdown=True)

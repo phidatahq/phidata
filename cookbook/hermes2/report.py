5f89ef5eb2eca464437ddd7b925f07a3f6f6c78a
Ashpreet Bedi <ashpreet@phidata.com>
2024-03-17 23:30:22 +0000

    update

--- cookbook/hermes2/report.py
+++ cookbook/hermes2/report.py
@@ -7,6 +7,6 @@ assistant = Assistant(
     llm=Hermes(model="adrienbrault/nous-hermes2pro:Q8_0"), tools=[DuckDuckGo(), WebsiteTools()], show_tool_calls=True
 )
 assistant.print_response(
-    "Produce a report about NousResearch. Search for 3 links, for each link provide a summary, the link, and a unique fact. After the table output make an ascii art of the overarching themes.",
+    "Produce a report about NousResearch. Search for 3 links, for each link provide a summary and a unique fact. Finally make an ascii art of NousResearch.",
     markdown=True,
 )

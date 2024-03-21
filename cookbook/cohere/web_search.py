457b7a33df496eaf457e20213d4110ee0bccc971
ysolanky <yash@phidata.com>
2024-03-21 12:48:25 -0400

    cohere-llm-phi-515

---
+++ cookbook/cohere/web_search.py
@@ -0,0 +1,10 @@
+from phi.assistant import Assistant
+from phi.tools.duckduckgo import DuckDuckGo
+from phi.llm.cohere import Cohere
+
+assistant = Assistant(
+    llm=Cohere(model="command-r"),
+    tools=[DuckDuckGo()],
+    show_tool_calls=True,
+)
+assistant.print_response("Share 1 story from france and 1 from germany?", markdown=True)
---
+++ phi/llm/cohere/cohere.py
@@ -0,0 +1,368 @@
+import json
+from textwrap import dedent
+from typing import Optional, List, Dict, Any
+
+
+from phi.llm.base import LLM
+from phi.llm.message import Message
+from phi.tools.function import FunctionCall
+from phi.utils.log import logger
+from phi.utils.timer import Timer
+from phi.utils.tools import (
+    get_function_call_for_tool_call,
+)
+
+try:
+    from cohere import Client as CohereClient
+    from cohere.responses.chat import StreamTextGeneration, ChatToolCallsGenerationEvent, Chat, StreamingChat, ToolCall
+except ImportError:
+    logger.error("`cohere` not installed")
+    raise
+
+
+class Cohere(LLM):
+    name: str = "cohere"
+    model: str = "command-r"
+    # # -*- Request parameters
+    temperature: Optional[float] = None
+    max_tokens: Optional[int] = None
+    top_k: Optional[int] = None
+    top_p: Optional[float] = None
+    frequency_penalty: Optional[float] = None
+    presence_penalty: Optional[float] = None
+    request_params: Optional[Dict[str, Any]] = None
+    # -*- Client parameters
+    api_key: Optional[str] = None
+    client_params: Optional[Dict[str, Any]] = None
+    # -*- Provide the client manually
+    cohere_client: Optional[CohereClient] = None
+
+    @property
+    def client(self) -> CohereClient:
+        if self.cohere_client:
+            return self.cohere_client
+
+        _client_params: Dict[str, Any] = {}
+        if self.api_key:
+            _client_params["api_key"] = self.api_key
+        return CohereClient(**_client_params)
+
+    @property
+    def api_kwargs(self) -> Dict[str, Any]:
+        _request_params: Dict[str, Any] = {}
+        if self.temperature:
+            _request_params["temperature"] = self.temperature
+        if self.max_tokens:
+            _request_params["max_tokens"] = self.max_tokens
+        if self.top_k:
+            _request_params["top_k"] = self.top_k
+        if self.top_p:
+            _request_params["top_p"] = self.top_p
+        if self.frequency_penalty:
+            _request_params["frequency_penalty"] = self.frequency_penalty
+        if self.presence_penalty:
+            _request_params["presence_penalty"] = self.presence_penalty
+        if self.request_params:
+            _request_params.update(self.request_params)
+        return _request_params
+
+    def get_tools(self) -> Optional[List[Dict[str, Any]]]:
+        if not self.functions:
+            return None
+
+        # Returns the tools in the format required by the Cohere API
+        return [
+            {
+                "name": f_name,
+                "description": function.description,
+                "parameter_definitions": {
+                    param_name: {
+                        "description": "",  # Assuming description is to be added or fetched from somewhere
+                        "type": param_info["type"] if isinstance(param_info["type"], str) else param_info["type"][0],
+                        "required": "null" not in param_info["type"],
+                    }
+                    for param_name, param_info in function.parameters.get("properties", {}).items()
+                },
+            }
+            for f_name, function in self.functions.items()
+        ]
+
+    def invoke(self, messages: List[Message], tool_results: Optional[List] = None) -> Chat:
+        api_kwargs: Dict[str, Any] = self.api_kwargs
+        api_kwargs["chat_history"] = []
+        user_message: List = []
+        # Track the last user message to prevent adding it to chat_history
+        last_user_message = None
+
+        if self.tools:
+            api_kwargs["tools"] = self.get_tools()
+
+        if tool_results:
+            api_kwargs["tool_results"] = tool_results
+
+        for m in messages:
+            if m.role == "system":
+                api_kwargs["preamble"] = m.content
+            elif m.role == "user":
+                if last_user_message is not None:
+                    # Append the previously tracked user message to chat_history before updating it
+                    api_kwargs["chat_history"].append({"role": "USER", "message": last_user_message})
+                # Update the last user message
+                last_user_message = m.content
+            else:
+                api_kwargs["chat_history"].append({"role": "CHATBOT", "message": m.content or ""})
+
+        if last_user_message:
+            user_message.append(last_user_message)
+
+        logger.debug(f"API KWARGS: {api_kwargs}")
+
+        return self.client.chat(model=self.model, message=" ".join(user_message), **api_kwargs)
+
+    def invoke_stream(self, messages: List[Message], tool_results: Optional[List] = None) -> StreamingChat:
+        api_kwargs: Dict[str, Any] = self.api_kwargs
+        api_kwargs["chat_history"] = []
+        user_message: List = []
+        # Track the last user message
+        last_user_message = None
+
+        if self.tools:
+            api_kwargs["tools"] = self.get_tools()
+
+        if tool_results:
+            api_kwargs["tool_results"] = tool_results
+
+        for m in messages:
+            if m.role == "system":
+                api_kwargs["preamble"] = m.content
+            elif m.role == "user":
+                if last_user_message is not None:
+                    # Append the previously tracked user message to chat_history before updating it
+                    api_kwargs["chat_history"].append({"role": "USER", "message": last_user_message})
+                last_user_message = m.content  # Update the last user message
+            else:
+                api_kwargs["chat_history"].append({"role": "CHATBOT", "message": m.content or ""})
+
+        if last_user_message:
+            user_message.append(last_user_message)
+
+        logger.debug(f"API KWARGS: {json.dumps(api_kwargs)}")
+
+        return self.client.chat(
+            model=self.model,
+            message=" ".join(user_message),
+            stream=True,
+            **api_kwargs,
+        )
+
+    def response(self, messages: List[Message], tool_results: Optional[List] = None) -> str:
+        logger.debug("---------- Cohere Response Start ----------")
+        # -*- Log messages for debugging
+        for m in messages:
+            m.log()
+
+        response_timer = Timer()
+        response_timer.start()
+        response: Chat = self.invoke(messages=messages, tool_results=tool_results)
+        response_timer.stop()
+        logger.debug(f"Time to generate response: {response_timer.elapsed:.4f}s")
+
+        logger.debug(f"Response: {response}")
+
+        # -*- Parse response
+        response_content = response.text
+
+        # -*- Create assistant message
+        assistant_message = Message(
+            role="assistant",
+            content=response_content or " ",
+        )
+
+        # -*- Create tool calls from response
+        tool_calls: List[Dict[str, Any]] = []
+        if response.tool_calls:
+            response_tool_calls: List[ToolCall] = response.tool_calls
+            for tools in response_tool_calls:
+                tool_calls.append(
+                    {
+                        "type": "function",
+                        "function": {
+                            "name": tools.name,
+                            "arguments": json.dumps(tools.parameters),
+                        },
+                    }
+                )
+
+        if len(tool_calls) > 0:
+            assistant_message.tool_calls = tool_calls
+
+        # -*- Update usage metrics
+        # Add response time to metrics
+        assistant_message.metrics["time"] = response_timer.elapsed
+        if "response_times" not in self.metrics:
+            self.metrics["response_times"] = []
+        self.metrics["response_times"].append(response_timer.elapsed)
+
+        # -*- Add assistant message to messages
+        messages.append(assistant_message)
+        assistant_message.log()
+
+        # -*- Run function call
+        if assistant_message.tool_calls is not None and self.run_tools:
+            final_response = ""
+            function_calls_to_run: List[FunctionCall] = []
+            for tool_call in assistant_message.tool_calls:
+                _function_call = get_function_call_for_tool_call(tool_call, self.functions)
+                if _function_call is None:
+                    messages.append(Message(role="user", content="Could not find function to call."))
+                    continue
+                if _function_call.error is not None:
+                    messages.append(Message(role="user", content=_function_call.error))
+                    continue
+                function_calls_to_run.append(_function_call)
+
+            if self.show_tool_calls:
+                if len(function_calls_to_run) == 1:
+                    final_response += f" - Running: {function_calls_to_run[0].get_call_str()}\n\n"
+                elif len(function_calls_to_run) > 1:
+                    final_response += "Running:"
+                    for _f in function_calls_to_run:
+                        final_response += f"\n - {_f.get_call_str()}"
+                    final_response += "\n\n"
+
+            function_call_results = self.run_function_calls(function_calls_to_run, role="user")
+
+            # Making sure the length of tool calls and function call results are the same to avoid unexpected behavior
+            if len(function_call_results) > 0 and len(response_tool_calls) == len(function_call_results):
+                # Constructs a list named tool_results, where each element is a dictionary that contains details of tool calls and their outputs.
+                # It pairs each tool call in response_tool_calls with its corresponding result in function_call_results.
+                tool_results = [
+                    {"call": tool_call, "outputs": [tool_call["parameters"], {"result": fn_result.content}]}
+                    for tool_call, fn_result in zip(response_tool_calls, function_call_results)
+                ]
+
+                messages.append(Message(role="user", content=" "))
+
+            # -*- Yield new response using results of tool calls
+            final_response += self.response(messages=messages, tool_results=tool_results)
+            return final_response
+        logger.debug("---------- Cohere Response End ----------")
+        # -*- Return content if no function calls are present
+        if assistant_message.content is not None:
+            return assistant_message.get_content_string()
+        return "Something went wrong, please try again."
+
+    def response_stream(self, messages: List[Message], tool_results: Optional[List] = None) -> Any:
+        logger.debug("---------- Cohere Response Start ----------")
+        # -*- Log messages for debugging
+        for m in messages:
+            m.log()
+
+        assistant_message_content = ""
+        tool_calls: List[Dict[str, Any]] = []
+        response_timer = Timer()
+        response_timer.start()
+        for response in self.invoke_stream(messages=messages, tool_results=tool_results):
+            # Detect if response is text
+            if isinstance(response, StreamTextGeneration):
+                if response.text is not None:
+                    assistant_message_content += response.text
+
+                    yield response.text
+
+            # Detect if response is a tool call
+            if isinstance(response, ChatToolCallsGenerationEvent):
+                response_tool_calls: List[ToolCall] = response.tool_calls
+
+                for tools in response_tool_calls:
+                    tool_calls.append(
+                        {
+                            "type": "function",
+                            "function": {
+                                "name": tools.name,
+                                "arguments": json.dumps(tools.parameters),
+                            },
+                        }
+                    )
+
+        response_timer.stop()
+        logger.debug(f"Time to generate response: {response_timer.elapsed:.4f}s")
+
+        # -*- Create assistant message
+        assistant_message = Message(
+            role="assistant",
+            content=assistant_message_content or "This is a tool call",
+        )
+
+        if len(tool_calls) > 0:
+            assistant_message.tool_calls = tool_calls
+
+        # -*- Update usage metrics
+        # Add response time to metrics
+        assistant_message.metrics["time"] = response_timer.elapsed
+        if "response_times" not in self.metrics:
+            self.metrics["response_times"] = []
+        self.metrics["response_times"].append(response_timer.elapsed)
+
+        # -*- Add assistant message to messages
+        messages.append(assistant_message)
+        assistant_message.log()
+
+        # -*- Parse and run function call
+        if assistant_message.tool_calls is not None and self.run_tools:
+            function_calls_to_run: List[FunctionCall] = []
+            for tool_call in assistant_message.tool_calls:
+                _function_call = get_function_call_for_tool_call(tool_call, self.functions)
+                if _function_call is None:
+                    messages.append(Message(role="user", content="Could not find function to call."))
+                    continue
+                if _function_call.error is not None:
+                    messages.append(Message(role="user", content=_function_call.error))
+                    continue
+                function_calls_to_run.append(_function_call)
+
+            if self.show_tool_calls:
+                if len(function_calls_to_run) == 1:
+                    yield f"- Running: {function_calls_to_run[0].get_call_str()}\n\n"
+                elif len(function_calls_to_run) > 1:
+                    yield "Running:"
+                    for _f in function_calls_to_run:
+                        yield f"\n - {_f.get_call_str()}"
+                    yield "\n\n"
+
+            function_call_results = self.run_function_calls(function_calls_to_run, role="user")
+
+            # Making sure the length of tool calls and function call results are the same to avoid unexpected behavior
+            if len(function_call_results) > 0 and len(response_tool_calls) == len(function_call_results):
+                # Constructs a list named tool_results, where each element is a dictionary that contains details of tool calls and their outputs.
+                # It pairs each tool call in response_tool_calls with its corresponding result in function_call_results.
+                tool_results = [
+                    {"call": tool_call, "outputs": [tool_call["parameters"], {"result": fn_result.content}]}
+                    for tool_call, fn_result in zip(response_tool_calls, function_call_results)
+                ]
+
+                logger.debug(f"Tool Results: {tool_results}")
+
+                messages.append(Message(role="user", content="This is a tool result"))
+
+            # -*- Yield new response using results of tool calls
+            yield from self.response_stream(messages=messages, tool_results=tool_results)
+
+        logger.debug("---------- Cohere Response End ----------")
+
+    def get_tool_call_prompt(self) -> Optional[str]:
+        if self.functions is not None and len(self.functions) > 0:
+            preamble = """
+            ## Task & Context
+            You help people answer their questions and other requests interactively. You will be asked a very wide array of requests on all kinds of topics. You will be equipped with a wide range of search engines or similar tools to help you, which you use to research your answer. You should focus on serving the user's needs as best you can, which will be wide-ranging.
+
+
+            ## Style Guide
+            Unless the user asks for a different style of answer, you should answer in full sentences, using proper grammar and spelling.
+            """
+            return dedent(preamble)
+
+        return None
+
+    def get_system_prompt_from_llm(self) -> Optional[str]:
+        return self.get_tool_call_prompt()

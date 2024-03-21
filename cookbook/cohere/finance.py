01915f69231edd4ce662a318979b91b3184445ba
ysolanky <yash@phidata.com>
2024-03-21 13:25:02 -0400

    update

--- cookbook/cohere/finance.py
+++ cookbook/cohere/finance.py
@@ -7,5 +7,5 @@ assistant = Assistant(
     tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True)],
     show_tool_calls=True,
 )
-assistant.print_response("Share the NVDA stock price and some analyst recommendations", markdown=True, stream=False)
+assistant.print_response("Share the NVDA stock price and some analyst recommendations", markdown=True)
 assistant.print_response("Summarize fundamentals for TSLA", markdown=True)

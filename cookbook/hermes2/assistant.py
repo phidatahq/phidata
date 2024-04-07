9f68391443e0adec1e0ff050beceb2e0cc3b4950
Ashpreet Bedi <ashpreet@phidata.com>
2024-04-07 17:46:34 +0100

    update hermes 2

--- cookbook/hermes2/assistant.py
+++ cookbook/hermes2/assistant.py
@@ -1,8 +1,8 @@
 from phi.assistant import Assistant
-from phi.llm.ollama import Ollama
+from phi.llm.ollama import Hermes
 
 assistant = Assistant(
-    llm=Ollama(model="adrienbrault/nous-hermes2pro:Q8_0"),
+    llm=Hermes(model="adrienbrault/nous-hermes2pro:Q8_0"),
     description="You help people with their health and fitness goals.",
 )
 assistant.print_response("Share a quick healthy breakfast recipe.", markdown=True)

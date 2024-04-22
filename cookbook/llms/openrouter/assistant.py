5769fdaea308feb117f5a86c1a40d8beb0665b2b
ysolanky <yash@phidata.com>
2024-04-22 12:01:48 -0400

    add-openrouter-support-phi-663

---
+++ cookbook/llms/openrouter/assistant.py
@@ -0,0 +1,8 @@
+from phi.assistant import Assistant
+from phi.llm.openrouter import OpenRouter
+
+assistant = Assistant(
+    llm=OpenRouter(model="mistralai/mistral-7b-instruct:free"),
+    description="You help people with their health and fitness goals.",
+)
+assistant.print_response("Share a 2 sentence quick and healthy breakfast recipe.", markdown=True)

f09e941af002dfd06f8b4a943d7fc27e732eb317
Ashpreet Bedi <ashpreet@phidata.com>
2024-04-24 23:25:35 +0100

    v2.3.81

--- cookbook/assistants/web_search.py
+++ cookbook/assistants/web_search.py
@@ -1,5 +1,5 @@
 from phi.assistant import Assistant
 from phi.tools.duckduckgo import DuckDuckGo
 
-assistant = Assistant(tools=[DuckDuckGo()], show_tool_calls=True, debug_mode=True)
+assistant = Assistant(tools=[DuckDuckGo()], show_tool_calls=True)
 assistant.print_response("Whats happening in France?", markdown=True)

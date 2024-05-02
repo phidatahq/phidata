4a3dc106a41dfd63a62ea4b6578a91f4ac636951
Ashpreet Bedi <ashpreet@phidata.com>
2024-05-02 01:42:04 +0100

    Add time to first token and tokens per second to ollama

--- cookbook/tools/email_tools.py
+++ cookbook/tools/email_tools.py
@@ -6,6 +6,15 @@ sender_email = "<sender_email>"
 sender_name = "<sender_name>"
 sender_passkey = "<sender_passkey>"
 
-assistant = Assistant(tools=[EmailTools(receiver_email=receiver_email, sender_email=sender_email, sender_name=sender_name, sender_passkey=sender_passkey)])
+assistant = Assistant(
+    tools=[
+        EmailTools(
+            receiver_email=receiver_email,
+            sender_email=sender_email,
+            sender_name=sender_name,
+            sender_passkey=sender_passkey,
+        )
+    ]
+)
 
 assistant.print_response("send an email to <receiver_email>")

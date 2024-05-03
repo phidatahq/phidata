435616f07cbbfaaf97b95b36fc896996f5bafa83
Ashpreet Bedi <ashpreet@phidata.com>
2024-05-03 10:12:37 +0100

    update websearch run

--- cookbook/assistants/web_search.py
+++ cookbook/assistants/web_search.py
@@ -3,3 +3,4 @@ from phi.tools.duckduckgo import DuckDuckGo
 
 assistant = Assistant(tools=[DuckDuckGo()], show_tool_calls=True, monitoring=True, debug_mode=True)
 assistant.print_response("Share 1 story from France?", markdown=True)
+assistant.print_response("Share 1 story from UK?", markdown=True)

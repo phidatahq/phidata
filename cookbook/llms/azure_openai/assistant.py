ac08a509d909744647afb3986f19f4ee503217a1
ysolanky <yash@phidata.com>
2024-05-13 22:09:40 -0400

    azureopenai-fix-phi-801

--- cookbook/llms/azure_openai/assistant.py
+++ cookbook/llms/azure_openai/assistant.py
@@ -2,7 +2,7 @@ from phi.assistant import Assistant
 from phi.llm.azure import AzureOpenAIChat
 
 assistant = Assistant(
-    llm=AzureOpenAIChat(model="gpt-35-turbo"),
+    llm=AzureOpenAIChat(model="yash-test-phi"),
     description="You help people with their health and fitness goals.",
 )
 assistant.print_response("Share a 2 sentence quick and healthy breakfast recipe.", markdown=True)

8a8df29bd8ad46b424c56b34e8cf1752fdfef9fa
Ashpreet Bedi <ashpreet@phidata.com>
2024-05-15 15:32:25 +0100

    README

--- cookbook/assistants/basic.py
+++ cookbook/assistants/basic.py
@@ -1,12 +1,8 @@
 from phi.assistant import Assistant
 from phi.llm.openai import OpenAIChat
-# from phi.llm.groq import Groq
-# from phi.llm.ollama import Ollama
 
 assistant = Assistant(
     llm=OpenAIChat(model="gpt-4o"),
-    # llm=Groq(model="llama3-70b-8192"),
-    # llm=Ollama(model="llama3"),
     description="You help people with their health and fitness goals.",
     instructions=["Recipes should be under 5 ingredients"],
 )

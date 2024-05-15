2c4903b7359cb4658e8966e2cd5b1a04f3304ec5
Ashpreet Bedi <ashpreet@phidata.com>
2024-05-15 15:26:09 +0100

    README

--- cookbook/assistants/basic.py
+++ cookbook/assistants/basic.py
@@ -1,8 +1,12 @@
 from phi.assistant import Assistant
 from phi.llm.openai import OpenAIChat
+# from phi.llm.groq import Groq
+# from phi.llm.ollama import Ollama
 
 assistant = Assistant(
-    llm=OpenAIChat(model="gpt-3.5-turbo"),
+    llm=OpenAIChat(model="gpt-4o"),
+    # llm=Groq(model="llama3-70b-8192"),
+    # llm=Ollama(model="llama3"),
     description="You help people with their health and fitness goals.",
     instructions=["Recipes should be under 5 ingredients"],
 )
--- cookbook/assistants/research.py
+++ cookbook/assistants/research.py
@@ -1,16 +1,22 @@
+from pathlib import Path
 from textwrap import dedent
 
 from phi.assistant import Assistant
-from phi.tools.duckduckgo import DuckDuckGo
-from phi.tools.newspaper4k import Newspaper4k
+from phi.llm.openai import OpenAIChat
+from phi.tools.exa import ExaTools
+
+cwd = Path(__file__).parent.resolve()
+scratch_dir = cwd.joinpath("scratch")
+if not scratch_dir.exists():
+    scratch_dir.mkdir(exist_ok=True, parents=True)
 
 assistant = Assistant(
-    tools=[DuckDuckGo(), Newspaper4k()],
+    llm=OpenAIChat(model="gpt-4o"),
+    tools=[ExaTools()],
     description="You are a senior NYT researcher writing an article on a topic.",
     instructions=[
-        "For the provided topic, search for the top 5 links.",
-        "Then read each URL and extract the article text. If a URL isn't available, ignore and move on.",
-        "Analyse and prepare an NYT worthy article based on the information.",
+        "For the provided topic, search for the top 10 links.",
+        "Read the results carefully and prepare an NYT worthy article.",
     ],
     add_datetime_to_instructions=True,
     expected_output=dedent("""\
@@ -37,8 +43,9 @@ assistant = Assistant(
     - [Title](url)
     </article_format>\
     """),
+    markdown=True,
     # show_tool_calls=True,
-    debug_mode=True,
-    save_output_to_file="news_article.md",
+    save_output_to_file=str(scratch_dir.joinpath("new_article.md")),
+    # debug_mode=True,
 )
-assistant.print_response("Latest developments in AI", markdown=True)
+assistant.print_response("OpenAI GPT-4o")

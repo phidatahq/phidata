53767f6f3e0c8a5a805aa24af8d25bb013a32f67
ysolanky <yash@phidata.com>
2024-06-07 12:41:03 -0400

    AzureOpenAIEmbedder update

--- phi/embedder/azure_openai.py
+++ phi/embedder/azure_openai.py
@@ -1,3 +1,4 @@
+from os import getenv
 from typing import Optional, Dict, List, Tuple, Any
 from typing_extensions import Literal
 
@@ -5,7 +6,7 @@ from phi.embedder.base import Embedder
 from phi.utils.log import logger
 
 try:
-    from openai import AzureOpenAI as OpenAIClient
+    from openai import AzureOpenAI as AzureOpenAIClient
     from openai.types.create_embedding_response import CreateEmbeddingResponse
 except ImportError:
     raise ImportError("`openai` not installed")
@@ -16,17 +17,20 @@ class AzureOpenAIEmbedder(Embedder):
     dimensions: int = 1536
     encoding_format: Literal["float", "base64"] = "float"
     user: Optional[str] = None
-    api_type: Optional[str] = "azure"
-    api_version: Optional[str] = None
-    api_key: Optional[str] = None
+    api_key: Optional[str] = getenv("AZURE_OPENAI_API_KEY")
+    api_version: str = getenv("AZURE_OPENAI_API_VERSION", "2024-02-01")
+    azure_endpoint: Optional[str] = getenv("AZURE_OPENAI_ENDPOINT")
+    azure_deployment: Optional[str] = getenv("AZURE_DEPLOYMENT")
+    base_url: Optional[str] = None
+    azure_ad_token: Optional[str] = None
+    azure_ad_token_provider: Optional[Any] = None
     organization: Optional[str] = None
-    azure_endpoint: Optional[str] = None
     request_params: Optional[Dict[str, Any]] = None
     client_params: Optional[Dict[str, Any]] = None
-    openai_client: Optional[OpenAIClient] = None
+    openai_client: Optional[AzureOpenAIClient] = None
 
     @property
-    def client(self) -> OpenAIClient:
+    def client(self) -> AzureOpenAIClient:
         if self.openai_client:
             return self.openai_client
 
@@ -35,10 +39,19 @@ class AzureOpenAIEmbedder(Embedder):
             _client_params["api_key"] = self.api_key
         if self.api_version:
             _client_params["api_version"] = self.api_version
+        if self.organization:
+            _client_params["organization"] = self.organization
         if self.azure_endpoint:
             _client_params["azure_endpoint"] = self.azure_endpoint
-        
-        return OpenAIClient(**_client_params)
+        if self.azure_deployment:
+            _client_params["azure_deployment"] = self.azure_deployment
+        if self.base_url:
+            _client_params["base_url"] = self.base_url
+        if self.azure_ad_token:
+            _client_params["azure_ad_token"] = self.azure_ad_token
+        if self.azure_ad_token_provider:
+            _client_params["azure_ad_token_provider"] = self.azure_ad_token_provider
+        return AzureOpenAIClient(**_client_params)
 
     def _response(self, text: str) -> CreateEmbeddingResponse:
         _request_params: Dict[str, Any] = {
@@ -52,7 +65,6 @@ class AzureOpenAIEmbedder(Embedder):
             _request_params["dimensions"] = self.dimensions
         if self.request_params:
             _request_params.update(self.request_params)
-        
         return self.client.embeddings.create(**_request_params)
 
     def get_embedding(self, text: str) -> List[float]:

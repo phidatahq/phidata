b8878932c78871ee44b559cb7b960c81acfb3013
VentCat <2546650292@qq.com>
2024-06-17 17:17:00 +0800

    Support 'FileTools' to write file encoding format

--- phi/tools/file.py
+++ phi/tools/file.py
@@ -24,11 +24,12 @@ class FileTools(Toolkit):
         if list_files:
             self.register(self.list_files)
 
-    def save_file(self, contents: str, file_name: str, overwrite: bool = True) -> str:
+    def save_file(self, contents: str, file_name: str, encoding: str = 'utf-8', overwrite: bool = True) -> str:
         """Saves the contents to a file called `file_name` and returns the file name if successful.
 
         :param contents: The contents to save.
         :param file_name: The name of the file to save to.
+        :param encoding: The file content encoding tag.
         :param overwrite: Overwrite the file if it already exists.
         :return: The file name if successful, otherwise returns an error message.
         """
@@ -39,7 +40,7 @@ class FileTools(Toolkit):
                 file_path.parent.mkdir(parents=True, exist_ok=True)
             if file_path.exists() and not overwrite:
                 return f"File {file_name} already exists"
-            file_path.write_text(contents)
+            file_path.write_text(contents,encoding=encoding)
             logger.info(f"Saved: {file_path}")
             return str(file_name)
         except Exception as e:

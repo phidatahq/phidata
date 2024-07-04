ed0a3d6ffac50eadae749d2ddd2919858388cf2d
ysolanky <yash@phidata.com>
2024-07-03 21:09:44 -0400

    gemini-llm-with-google-ai-studio-phi-899

---
+++ cookbook/llms/google/assistant.py
@@ -0,0 +1,6 @@
+from phi.assistant import Assistant
+from phi.llm.google import Gemini
+from phi.tools.duckduckgo import DuckDuckGo
+
+assistant = Assistant(llm=Gemini(), tools=[DuckDuckGo()], debug_mode=True, show_tool_calls=True)
+assistant.print_response("Whats happening in France?", markdown=True)

2667cae988ec77fa0bcf14a5b63324ae10086516
CorrM <islamnofl.official@gmail.com>
2024-07-24 18:27:49 +0300

    format

--- tests/llms/ollama/structured_output.py
+++ tests/llms/ollama/structured_output.py
@@ -10,7 +10,9 @@ from phi.llm.ollama import Ollama
 class MovieScript(BaseModel):
     setting: str = Field(..., description="Provide a nice setting for a blockbuster movie.")
     ending: str = Field(..., description="Ending of the movie. If not available, provide a happy ending.")
-    genre: str = Field(..., description="Genre of the movie. If not available, select action, thriller or romantic comedy.")
+    genre: str = Field(
+        ..., description="Genre of the movie. If not available, select action, thriller or romantic comedy."
+    )
     name: str = Field(..., description="Give a name to this movie")
     characters: list[str] = Field(..., description="Name of characters for this movie.")
     storyline: str = Field(..., description="3 sentence storyline for the movie. Make it exciting!")

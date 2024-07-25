1a7b407cbcae44c4045d20f2d6861ad8724d7a48
ysolanky <yash@phidata.com>
2024-07-25 13:38:44 -0400

    Update

--- cookbook/tools/crawl4ai_tools.py
+++ cookbook/tools/crawl4ai_tools.py
@@ -1,5 +1,5 @@
 from phi.assistant import Assistant
-from phi.tools.crawl4ai_tools import Crawl4aiTools
+from phi.tools.crawl4ai import Crawl4aiTools
 
 assistant = Assistant(tools=[Crawl4aiTools(max_length=None)], show_tool_calls=True, debug_mode=True)
 assistant.print_response("Tell me about https://github.com/phidatahq/phidata.", markdown=True)

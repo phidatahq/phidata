fbf4152c7271690ca6d7f4d63064e4d5f66729c7
jacob weiss <jaweiss2305@gmail.com>
2024-08-28 12:50:01 -0600

    remove sentence transformers

--- cookbook/llms/huggingface/sentence_transformer_embeddings.py
+++
@@ -1,6 +0,0 @@
-from phi.embedder.sentence_transformer import SentenceTransformerEmbedder
-
-embeddings = SentenceTransformerEmbedder().get_embedding("Embed me")
-
-print(f"Embeddings: {embeddings[:5]}")
-print(f"Dimensions: {len(embeddings)}")

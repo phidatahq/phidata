c2459865582300d98feb596c353e72fcd05f7a4a
ysolanky <yash@phidata.com>
2024-09-05 09:43:49 -0400

    fix-fireworks-streaming-phi-1032

--- phi/llm/fireworks/fireworks.py
+++ phi/llm/fireworks/fireworks.py
@@ -1,7 +1,9 @@
 from os import getenv
-from typing import Optional
+from typing import Optional, List, Iterator
 
+from phi.llm.message import Message
 from phi.llm.openai.like import OpenAILike
+from openai.types.chat.chat_completion_chunk import ChatCompletionChunk
 
 
 class Fireworks(OpenAILike):
@@ -9,3 +11,11 @@ class Fireworks(OpenAILike):
     model: str = "accounts/fireworks/models/firefunction-v1"
     api_key: Optional[str] = getenv("FIREWORKS_API_KEY")
     base_url: str = "https://api.fireworks.ai/inference/v1"
+
+    def invoke_stream(self, messages: List[Message]) -> Iterator[ChatCompletionChunk]:
+        yield from self.get_client().chat.completions.create(
+            model=self.model,
+            messages=[m.to_dict() for m in messages],  # type: ignore
+            stream=True,
+            **self.api_kwargs,
+        )  # type: ignore

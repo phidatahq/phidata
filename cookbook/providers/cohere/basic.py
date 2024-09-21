27ac497857f3f1b38da8fc6c66b7f3b1b9c02804
jacob weiss <jaweiss2305@gmail.com>
2024-09-21 06:21:53 -0600

    remove assistant from cookbook examples. this already exists in llm examples

--- cookbook/providers/cohere/basic.py
+++
@@ -1,8 +0,0 @@
-from phi.assistant import Assistant
-from phi.model.cohere import CohereChat
-
-assistant = Assistant(
-    model=CohereChat(model="command-r"),
-    description="You help people with their health and fitness goals.",
-)
-assistant.print_response("Share a quick healthy breakfast recipe.", markdown=True)

adff2e3aa83b6ea10fcc58f459007fbc1df646e9
jacob weiss <jaweiss2305@gmail.com>
2024-09-23 18:45:11 -0700

    add basic and basic stream cookbook examples

---
+++ cookbook/providers/openai/basic.py
@@ -0,0 +1,14 @@
+from phi.agent import Agent, RunResponse
+from phi.model.openai import OpenAIChat
+
+agent = Agent(
+    model=OpenAIChat(model="gpt-4o"),
+    instructions=["Respond in a southern tone"]
+    # debug_mode=True,
+)
+
+
+# Return the result as a string
+run: RunResponse = agent.run("Explain simulation theory")  # type: ignore
+
+print(run.content)
---
+++ cookbook/providers/openai/basic_stream.py
@@ -0,0 +1,12 @@
+from phi.agent import Agent, RunResponse
+from phi.model.openai import OpenAIChat
+
+agent = Agent(
+    model=OpenAIChat(model="gpt-4-turbo"),
+    instructions=["Respond in a southern tone"]
+    # debug_mode=True,
+)
+
+
+# Return the result as a string
+agent.print_response("What is a type 2 civilization? How close are we?", stream=True)  # type: ignore

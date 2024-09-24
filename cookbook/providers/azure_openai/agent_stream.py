3b102ba92bb8f258be0a806d16d953516d7341df
jacob weiss <jaweiss2305@gmail.com>
2024-09-23 17:27:35 -0700

    update example with .env

--- cookbook/providers/azure_openai/agent_stream.py
+++ cookbook/providers/azure_openai/agent_stream.py
@@ -2,12 +2,24 @@ from phi.agent import Agent
 from phi.model.azure import AzureOpenAIChat
 from phi.tools.duckduckgo import DuckDuckGo
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+azure_model = AzureOpenAIChat(
+    model = os.getenv("AZURE_OPENAI_MODEL_NAME"),
+    api_key = os.getenv("AZURE_OPENAI_API_KEY"),
+    azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"),
+    azure_deployment = os.getenv("AZURE_OPENAI_DEPLOYMENT"),
+)
+
 agent = Agent(
-    model=AzureOpenAIChat(
-        model="gpt-4o"
-    ),
+    model=azure_model,
     tools=[DuckDuckGo()],
     show_tool_calls=True,
     # debug_mode=True,
 )
+
 agent.print_response("Whats happening in France?", markdown=True, stream=True)
+

2f229314d5427147274f699875bfa44f27cc016a
jacob weiss <jaweiss2305@gmail.com>
2024-09-25 07:56:11 -0400

    fix merge conflict

--- phi/model/azure/__init__.py
+++ phi/model/azure/__init__.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 from phi.model.azure.openai_chat import AzureOpenAIChat
-=======
-from phi.model.azure.openai_chat import AzureOpenAIChat
->>>>>>> 46a54c3b2a88d06f8fb9d31b6db168f1c8fef00c

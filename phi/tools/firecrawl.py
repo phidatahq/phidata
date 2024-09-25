344ca16e8456cc6e2cf0c91b3c79aac6adb296c1
ayush0054 <ayush5april@gmail.com>
2024-09-26 01:57:30 +0530

    removed wait_until_done from crawl

--- phi/tools/firecrawl.py
+++ phi/tools/firecrawl.py
@@ -17,7 +17,7 @@ class FirecrawlTools(Toolkit):
         limit: int = 10,
         scrape: bool = True,
         crawl: bool = False,
-        wait_until_done: bool = True,
+
     ):
         super().__init__(name="firecrawl_tools")
 
@@ -25,7 +25,7 @@ class FirecrawlTools(Toolkit):
         self.formats: Optional[List[str]] = formats
         self.limit: int = limit
         self.app: FirecrawlApp = FirecrawlApp(api_key=self.api_key)
-        self.wait_until_done: bool = wait_until_done
+
         if scrape:
             self.register(self.scrape_website)
         if crawl:
@@ -50,7 +50,7 @@ class FirecrawlTools(Toolkit):
         scrape_result = self.app.scrape_url(url, params=params)
         return json.dumps(scrape_result)
 
-    def crawl_website(self, url: str, limit: Optional[int] = None, wait_until_done: Optional[bool] = None) -> str:
+    def crawl_website(self, url: str, limit: Optional[int] = None) -> str:
         """Use this function to Crawls a website using Firecrawl.
 
         Args:
@@ -69,7 +69,6 @@ class FirecrawlTools(Toolkit):
             params["limit"] = self.limit or limit
             if self.formats:
                 params["scrapeOptions"] = {"formats": self.formats}
-        wait_until_done = wait_until_done if wait_until_done is not None else self.wait_until_done
-        print(wait_until_done)
-        crawl_result = self.app.crawl_url(url, params=params, wait_until_done=wait_until_done, poll_interval=30)
+
+        crawl_result = self.app.crawl_url(url, params=params,  poll_interval=30)
         return json.dumps(crawl_result)

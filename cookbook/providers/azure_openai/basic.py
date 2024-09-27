22869bc160de5b763b48a6c2f1e43ea7f705e58d
jacob weiss <jaweiss2305@gmail.com>
2024-09-27 17:12:09 -0600

    add cookbook example

---
+++ cookbook/providers/azure_openai/basic.py
@@ -0,0 +1,24 @@
+import os
+
+from phi.agent import Agent, RunResponse  # noqa
+from phi.model.azure import AzureOpenAIChat
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+azure_model = AzureOpenAIChat(
+    id=os.getenv("AZURE_OPENAI_MODEL_NAME"),
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+    azure_deployment=os.getenv("AZURE_OPENAI_DEPLOYMENT"),
+)
+
+agent = Agent(model=azure_model, instructions=["Respond in a southern tone"], markdown=True)
+
+# Get the response in a variable
+# run: RunResponse = agent.run("Explain simulation theory")
+# print(run.content)
+
+# Print the response on the terminal
+agent.print_response("Explain simulation theory")
---
+++ cookbook/providers/azure_openai/basic_stream.py
@@ -0,0 +1,26 @@
+import os
+
+from typing import Iterator  # noqa
+from phi.agent import Agent, RunResponse  # noqa
+from phi.model.azure import AzureOpenAIChat
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+azure_model = AzureOpenAIChat(
+    id=os.getenv("AZURE_OPENAI_MODEL_NAME"),
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+    azure_deployment=os.getenv("AZURE_OPENAI_DEPLOYMENT"),
+)
+
+agent = Agent(model=azure_model, instructions=["Respond in a southern tone"], markdown=True)
+
+# Get the response in a variable
+# run_response: Iterator[RunResponse] = agent.run("Explain simulation theory", stream=True)
+# for chunk in run_response:
+#     print(chunk.content)
+
+# Print the response on the terminal
+agent.print_response("Explain simulation theory", stream=True)

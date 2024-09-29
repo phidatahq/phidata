f0cd26e661b99f2e1ed063d151aee00867a1d8d4
Ashpreet Bedi <ashpreet@phidata.com>
2024-09-29 16:12:32 +0100

    Workflow

--- cookbook/workflows/news_article.py
+++ cookbook/workflows/news_article.py
@@ -7,7 +7,6 @@ from phi.agent import Agent, AgentResponse
 from phi.model.openai import OpenAIChat
 from phi.workflow import Workflow
 from phi.tools.duckduckgo import DuckDuckGo
-from phi.tools.newspaper4k import Newspaper4k
 from phi.utils.log import logger
 
 
@@ -28,46 +27,6 @@ class WriteNewsReport(Workflow):
         tools=[DuckDuckGo()],
         description="Given a topic, search for 5 articles and return the 3 most relevant articles.",
         response_model=NewsArticles,
-    )
-
-    writer: Agent = Agent(
-        name="Writer",
-        tools=[Newspaper4k()],
-        description="You are a Senior NYT Editor and your task is to write a NYT cover story due tomorrow.",
-        instructions=[
-            "You will be provided with news articles and their links.",
-            "Carefully read each article and think about the contents",
-            "Then generate a final New York Times worthy article in the <article_format> provided below.",
-            "Break the article into sections and provide key takeaways at the end.",
-            "Make sure the title is catchy and engaging.",
-            "Give the section relevant titles and provide details/facts/processes in each section."
-            "Ignore articles that you cannot read or understand.",
-            "REMEMBER: you are writing for the New York Times, so the quality of the article is important.",
-        ],
-        expected_output=dedent("""\
-        An engaging, informative, and well-structured article in the following format:
-        <article_format>
-        ## Engaging Article Title
-
-        ### Overview
-        {give a brief introduction of the article and why the user should read this report}
-        {make this section engaging and create a hook for the reader}
-
-        ### Section 1
-        {break the article into sections}
-        {provide details/facts/processes in this section}
-
-        ... more sections as necessary...
-
-        ### Takeaways
-        {provide key takeaways from the article}
-
-        ### References
-        - [Title](url)
-        - [Title](url)
-        - [Title](url)
-        </article_format>
-        """),
     )
 
     def run(self, topic: str):

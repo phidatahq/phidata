e9fa0560584b8937890219fdfc5aaa85e091aabc
Ashpreet Bedi <ashpreet@phidata.com>
2024-10-01 12:35:10 +0100

    update

--- cookbook/agents/web_search.py
+++ cookbook/agents/web_search.py
@@ -2,5 +2,5 @@ from phi.agent import Agent
 from phi.model.openai import OpenAIChat
 from phi.tools.duckduckgo import DuckDuckGo
 
-web_search_agent = Agent(model=OpenAIChat(id="gpt-4o"), tools=[DuckDuckGo()], show_tool_calls=True, markdown=True)
-web_search_agent.print_response("Share 3 news stories from France")
+agent = Agent(model=OpenAIChat(id="gpt-4o"), tools=[DuckDuckGo()], show_tool_calls=True, markdown=True)
+agent.print_response("Whats happening in France?", stream=True)

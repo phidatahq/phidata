abcf53c11298bcbac867046d8b03e5712cdffc45
Ashpreet Bedi <ashpreet@phidata.com>
2024-10-03 12:32:39 +0100

    updates

--- cookbook/agents/finance_agent.py
+++ cookbook/agents/finance_agent.py
@@ -5,7 +5,7 @@ from phi.model.openai import OpenAIChat
 from phi.tools.yfinance import YFinanceTools
 
 agent = Agent(
-    model=OpenAIChat(id="gpt-4o", store=True),
+    model=OpenAIChat(id="gpt-4o"),
     tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, company_info=True, company_news=True)],
     instructions=["Use tables where possible"],
     show_tool_calls=True,

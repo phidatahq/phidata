0836a265741e05cf2b28535bfb5fbf320bdcff66
Ashpreet Bedi <ashpreet@phidata.com>
2024-10-07 01:08:28 +0100

    Fix token counts

---
+++ cookbook/agents/personalized_memories_and_summaries.py
@@ -0,0 +1,41 @@
+from rich.pretty import pprint
+
+from phi.agent import Agent, AgentMemory
+from phi.model.openai import OpenAIChat
+from phi.memory.db.postgres import PgMemoryDb
+from phi.storage.agent.postgres import PgAgentStorage
+
+db_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
+agent = Agent(
+    model=OpenAIChat(id="gpt-4o"),
+    # Store the memories and summary in a database
+    memory=AgentMemory(db=PgMemoryDb(table_name="agent_memory", db_url=db_url), create_user_memories=True, create_session_summary=True),
+    # Store runs in a database
+    storage=PgAgentStorage(table_name="personalized_agent_sessions", db_url=db_url),
+    # Show debug logs so you can see the memory being created
+    # debug_mode=True,
+)
+
+# -*- Share personal information
+agent.print_response("My name is john billings?", stream=True)
+# -*- Print memories
+pprint(agent.memory.memories)
+# -*- Print summary
+pprint(agent.memory.summary)
+
+# -*- Share personal information
+agent.print_response("I live in nyc?", stream=True)
+# -*- Print memories
+pprint(agent.memory.memories)
+# -*- Print summary
+pprint(agent.memory.summary)
+
+# -*- Share personal information
+agent.print_response("I'm going to a concert tomorrow?", stream=True)
+# -*- Print memories
+pprint(agent.memory.memories)
+# -*- Print summary
+pprint(agent.memory.summary)
+
+# Ask about the conversation
+agent.print_response("What have we been talking about, do you know my name?", stream=True)

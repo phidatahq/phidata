a1b804012538a054c9a9be35b7ae0d68d1acc87e
ysolanky <yash@phidata.com>
2024-10-09 15:20:55 -0400

    update

--- cookbook/tools/exa_tools.py
+++ cookbook/tools/exa_tools.py
@@ -5,7 +5,7 @@ from phi.tools.exa import ExaTools
 
 os.environ["EXA_API_KEY"] = "your api key"
 
-assistant = Agent(
+agent = Agent(
     tools=[ExaTools(include_domains=["cnbc.com", "reuters.com", "bloomberg.com"])], show_tool_calls=True
 )
-assistant.print_response("Search for AAPL news", debug_mode=True, markdown=True)
+agent.print_response("Search for AAPL news", debug_mode=True, markdown=True)

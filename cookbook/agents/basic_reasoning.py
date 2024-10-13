25cd3aff192266572f1c3beed529be39805bdb0f
Ashpreet Bedi <ashpreet@phidata.com>
2024-10-13 21:05:40 +0100

    Add more reasoning tasks

---
+++ cookbook/agents/basic_reasoning.py
@@ -0,0 +1,15 @@
+from phi.agent import Agent
+from phi.model.openai import OpenAIChat
+from phi.cli.console import console
+
+regular_agent = Agent(model=OpenAIChat(id="gpt-4o"), markdown=True)
+reasoning_agent = Agent(
+    model=OpenAIChat(id="gpt-4o-2024-08-06"), reasoning=True, markdown=True, structured_outputs=True
+)
+
+task = "How many 'r' are in the word 'supercalifragilisticexpialidocious'?"
+
+console.rule("[bold green]Regular Agent[/bold green]")
+regular_agent.print_response(task, stream=True)
+console.rule("[bold yellow]Reasoning Agent[/bold yellow]")
+reasoning_agent.print_response(task, stream=True)

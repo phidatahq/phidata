76483046faa5fea1d9b38832e87ba46431ef7d2f
Ashpreet Bedi <ashpreet@phidata.com>
2024-10-13 21:02:17 +0100

    Add more reasoning tasks

--- cookbook/reasoning/finance_agent.py
+++ cookbook/reasoning/finance_agent.py
@@ -9,7 +9,6 @@ reasoning_agent = Agent(
     show_tool_calls=True,
     markdown=True,
     reasoning=True,
-    debug_mode=True,
 )
 
 reasoning_agent.print_response("Write a report comparing NVDA to TSLA", stream=True, show_full_reasoning=True)

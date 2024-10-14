431078208b1fa658760bb5e3c933b8039c5374f6
ysolanky <yash@phidata.com>
2024-10-14 01:24:16 -0400

    tools-cookbook-update-phi-1566

--- cookbook/tools/youtube_tools.py
+++ cookbook/tools/youtube_tools.py
@@ -5,6 +5,5 @@ agent = Agent(
     tools=[YouTubeTools()],
     show_tool_calls=True,
     description="You are a YouTube agent. Obtain the captions of a YouTube video and answer questions.",
-    debug_mode=True,
 )
 agent.print_response("Summarize this video https://www.youtube.com/watch?v=Iv9dewmcFbs&t", markdown=True)

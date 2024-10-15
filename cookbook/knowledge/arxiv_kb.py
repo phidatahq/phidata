5d9019e0dc8ae997cf15c6563a27489715653eec
ysolanky <yash@phidata.com>
2024-10-15 07:22:16 -0400

    kb-cookbooks-phi-1641

---
+++ cookbook/knowledge/arxiv_kb.py
@@ -0,0 +1,26 @@
+from phi.agent import Agent
+from phi.knowledge.arxiv import ArxivKnowledgeBase
+from phi.vectordb.pgvector import PgVector
+
+db_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
+
+# Create a knowledge base with the ArXiv documents
+knowledge_base = ArxivKnowledgeBase(
+    queries=["Generative AI", "Machine Learning"],
+    # Table name: ai.arxiv_documents
+    vector_db=PgVector(
+        collection="arxiv_documents",
+        db_url=db_url,
+    ),
+)
+# Load the knowledge base
+knowledge_base.load(recreate=False)
+
+# Create an agent with the knowledge base
+agent = Agent(
+    knowledge_base=knowledge_base,
+    add_references_to_prompt=True,
+)
+
+# Ask the agent about the knowledge base
+agent.print_response("Ask me about something from the knowledge base", markdown=True)
---
+++ cookbook/knowledge/website_kb.py
@@ -0,0 +1,28 @@
+from phi.agent import Agent
+from phi.knowledge.website import WebsiteKnowledgeBase
+from phi.vectordb.pgvector import PgVector
+
+db_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
+
+# Create a knowledge base with the seed URLs
+knowledge_base = WebsiteKnowledgeBase(
+    urls=["https://docs.phidata.com/introduction"],
+    # Number of links to follow from the seed URLs
+    max_links=10,
+    # Table name: ai.website_documents
+    vector_db=PgVector(
+        table_name="website_documents",
+        db_url=db_url,
+    ),
+)
+# Load the knowledge base
+knowledge_base.load(recreate=False)
+
+# Create an agent with the knowledge base
+agent = Agent(
+    knowledge_base=knowledge_base,
+    add_references_to_prompt=True,
+)
+
+# Ask the agent about the knowledge base
+agent.print_response("How does phidata work?")

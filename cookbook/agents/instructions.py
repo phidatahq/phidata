97593b197aec3171c39882ae593d78d744b2a400
Ashpreet Bedi <ashpreet@phidata.com>
2024-10-23 17:44:16 +0100

    v2.5.19

--- cookbook/agents/instructions.py
+++
@@ -1,9 +0,0 @@
-from phi.agent import Agent
-
-agent = Agent(
-    description="You are a famous short story writer asked to write for a magazine",
-    instructions=["You are a pilot on a plane flying from Hawaii to Japan."],
-    markdown=True,
-    debug_mode=True,
-)
-agent.print_response("Tell me a 2 sentence horror story.", stream=True)

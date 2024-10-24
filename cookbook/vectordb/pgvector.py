e70607f189fe6ff807c40df69683eed2391e56e1
ysolanky <yash@phidata.com>
2024-10-24 12:11:23 -0400

    update-contribution-guide-phi-1764

---
+++ cookbook/vectordb/pgvector.py
@@ -0,0 +1,14 @@
+from phi.agent import Agent
+from phi.knowledge.pdf import PDFUrlKnowledgeBase
+from phi.vectordb.pgvector import PgVector
+
+db_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
+
+knowledge_base = PDFUrlKnowledgeBase(
+    urls=["https://phi-public.s3.amazonaws.com/recipes/ThaiRecipes.pdf"],
+    vector_db=PgVector(table_name="recipes", db_url=db_url),
+)
+knowledge_base.load(recreate=False)  # Comment out after first run
+
+agent = Agent(knowledge_base=knowledge_base, use_tools=True, show_tool_calls=True)
+agent.print_response("How to make Thai curry?", markdown=True)

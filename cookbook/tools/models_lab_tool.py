a64be27be352e351a9255084d26bcf17c0940fbe
Manthan Gupta <manthangupta109@gmail.com>
2024-11-06 15:29:53 +0530

    fix formatting

--- cookbook/tools/models_lab_tool.py
+++ cookbook/tools/models_lab_tool.py
@@ -4,9 +4,6 @@ from phi.agent import Agent
 from phi.tools.models_labs import ModelsLabs
 
 # Create an Agent with the ModelsLabs tool
-agent = Agent(
-    tools=[ModelsLabs()],
-   name="ModelsLabs Agent"
-)
+agent = Agent(tools=[ModelsLabs()], name="ModelsLabs Agent")
 
 agent.print_response("Generate a video of a beautiful sunset over the ocean", markdown=True)

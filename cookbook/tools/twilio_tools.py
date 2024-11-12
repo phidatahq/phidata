f2f35517e3a1dc24913916ea27ad4aa62089cb70
Jayesh <jayeshparmar9829@gmail.com>
2024-11-12 16:51:02 +0530

    added variables for sender and receiver phone number

--- cookbook/tools/twilio_tools.py
+++ cookbook/tools/twilio_tools.py
@@ -1,6 +1,6 @@
 from phi.agent import Agent
-from phi.tools.twilio import TwilioTools
 from phi.model.openai import OpenAIChat
+from phi.tools.twilio import TwilioTools
 
 """
 Example showing how to use the Twilio Tools with Phi.
@@ -34,4 +34,9 @@ agent = Agent(
     markdown=True,
 )
 
-agent.print_response("Can you send an SMS saying 'Your package has arrived' to +1234567890?")
+sender_phone_number = "+1234567890"
+receiver_phone_number = "+1234567890"
+
+agent.print_response(
+    f"Can you send an SMS saying 'Your package has arrived' to {receiver_phone_number} from {sender_phone_number}?"
+)

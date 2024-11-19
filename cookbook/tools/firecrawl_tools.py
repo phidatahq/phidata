23cc69d34938cd863c7d269bdf985ecd35a9f1e2
Abhishek Ranjan <abhishek.ranjan@sprouts.ai>
2024-11-19 20:18:16 +0530

    chore: address requested changes

--- cookbook/tools/firecrawl_tools.py
+++ cookbook/tools/firecrawl_tools.py
@@ -1,31 +1,34 @@
 from phi.agent import Agent
 from phi.tools.firecrawl import FirecrawlTools
 
+agent = Agent(
+    tools=[FirecrawlTools(scrape=True, crawl=True, async_crawl=True, map=True, limit=25)],
+    show_tool_calls=True,
+    reasoning=True,
+    markdown=True,
+)
 
-def agent_with_predefined_toolkit(url: str):
-    agent = Agent(
-        tools=[FirecrawlTools(map=True, scrape=True)],
-        show_tool_calls=True,
-        markdown=True,
-        reasoning=True,
-    )
+
+def map_website_example(url: str):
     agent.print_response(f"Fetch all the blog links from the website: {url}")
-    agent.print_response(
-        "scrape and summarize the following website: https://www.firecrawl.dev/blog/using-structured-output-and-json-strict-mode-openai"
-    )
+
+
+def scrape_example(url: str):
+    agent.print_response(f"scrape and summarize the following website: {url}")
 
 
 def crawl_example(url: str):
-    agent = Agent(
-        tools=[FirecrawlTools(scrape=True, crawl=True, async_crawl=True, limit=25)],
-        show_tool_calls=True,
-        reasoning=True,
-        markdown=True,
-    )
-    agent.print_response(f"Crawl and analyze the main use cases and  insights using asynchronous crawling: {url}")
+    agent.print_response(f"Crawl and analyze the main use cases and insights using asynchronous crawling: {url}")
+
+
+def async_crawl_example(url: str):
+    agent.print_response(f"Crawl and analyze the main use cases and insights using asynchronous crawling: {url}")
 
 
 if __name__ == "__main__":
-    url = "https://www.firecrawl.dev/blog"
-    agent_with_predefined_toolkit(url)
+    url = "https://docs.phidata.com/agents"
+    map_website_example(url)
     crawl_example(url)
+    async_crawl_example(url)
+    scrape_url = "https://docs.phidata.com/workflows"
+    scrape_example(scrape_url)

bc568a8ff0dffa3bb38d153a8cbc4edbe9f61659
ysolanky <yash@phidata.com>
2024-12-04 18:19:30 -0500

    Workflow sessions

--- phi/workflow/workflow.py
+++ phi/workflow/workflow.py
@@ -7,6 +7,7 @@ from typing import Any, Optional, Callable, Dict
 
 from pydantic import BaseModel, Field, ConfigDict, field_validator, PrivateAttr
 
+from phi.agent import Agent
 from phi.run.response import RunResponse, RunEvent  # noqa: F401
 from phi.memory.workflow import WorkflowMemory, WorkflowRun
 from phi.storage.workflow import WorkflowStorage
@@ -304,6 +305,13 @@ class Workflow(BaseModel):
         else:
             # This will log an error when called
             self._subclass_run = self.run
+
+    def model_post_init(self, __context: Any) -> None:
+        super().model_post_init(__context)
+        for field_name, field in self.__fields__.items():
+            value = getattr(self, field_name)
+            if isinstance(value, Agent):
+                value.session_id = self.session_id
 
     def log_workflow_session(self):
         logger.debug(f"*********** Logging WorkflowSession: {self.session_id} ***********")

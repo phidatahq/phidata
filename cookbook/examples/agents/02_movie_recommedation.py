6fb7b9c382f8247f3465352d29aba5c08f816695
unnati914 <unnati002btit19@igdtuw.ac.in>
2024-12-05 00:56:53 +0530

    some fixes

--- cookbook/examples/agents/02_movie_recommedation.py
+++ cookbook/examples/agents/02_movie_recommedation.py
@@ -12,14 +12,15 @@ movie_recommedation_agent = Agent(
     model=OpenAIChat(id="gpt-4o"),
     description="You are a movie recommendation agent. You search movies by doing Exa search then scraping through a website.",
     instructions=[
-        "Search exa for movies",
+        "Search exa for movies according to user input",
         "Then Scrape the website https://www.imdb.com/",
-        "Give a result with rating of the movie, it's genre, description, recommended age to watch,language, similar trailers like of recommended one and upcoming movies",
-        "Do not give the IMDB link to the movie"
+        "Give a result with rating of the movie, it's genre, description, recommended age to watch,language, similar trailers like of recommended one and upcoming movies, best movies to watch for that particular year",
+        "Suggest best movies to watch for that particular year "
+        "Do not give the IMDB link to the movie",
     ],
     show_tool_calls=True,
     debug_mode=True,      
     markdown=True,         
     stream=True 
 )
-movie_recommedation_agent.print_response("Suggest some movies for 10 year old to watch")
+movie_recommedation_agent.print_response("suggest best action movie")

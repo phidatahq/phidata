fc596603254f0d3e6c2dd9c6c5ad502016423ecf
unnati914 <unnati002btit19@igdtuw.ac.in>
2024-12-05 21:51:24 +0530

    agent name

--- cookbook/examples/agents/02_movie_recommedation.py
+++ cookbook/examples/agents/02_movie_recommedation.py
@@ -4,6 +4,7 @@ from phi.tools.apify import ApifyTools
 from phi.tools.exa import ExaTools
 
 movie_recommedation_agent = Agent(
+    name="PopcornPal",
     tools=[
         ExaTools(),
         ApifyTools() 
@@ -11,10 +12,8 @@ movie_recommedation_agent = Agent(
     model=OpenAIChat(id="gpt-4o"),
     description="You are a movie recommendation agent. You search movies by doing Exa search then scraping through a website.",
     instructions=[
-        "Search exa for movies according to user input",
-        "Then Scrape the website https://www.imdb.com/",
+        "Crawl the website https://www.imdb.com/",
         "Give a result with rating of the movie, it's genre, description, recommended age to watch,language, similar trailers like of recommended one and upcoming movies, best movies to watch for that particular year, remember to give the output in this format",
-        "Suggest best movies to watch for that particular year "
         "Do not give the IMDB link to the movie",
         "Use tables to give the result"
     ],

dec0bc16d5013d09a064c2a15c5664e3f78796a7
unnati914 <unnati002btit19@igdtuw.ac.in>
2024-12-05 23:46:27 +0530

    fixes

--- cookbook/examples/agents/01_ai_recipe_creator.py
+++ cookbook/examples/agents/01_ai_recipe_creator.py
@@ -6,6 +6,7 @@ from phi.tools.apify import ApifyTools
 from phi.tools.exa import ExaTools
 
 ai_recipe_agent = Agent(
+    name="RecipeGenie",
     tools=[
         ExaTools(),
         ApifyTools(web_scraper=True),
@@ -15,15 +16,12 @@ ai_recipe_agent = Agent(
     description="You are a recipe suggestor. Suggest recipes based on input ingredients by doing exa Search and summarizing from YouTube videos.",
     instructions=[
         "Search exa for recipes",
-        "Then scrape the website https://www.allrecipes.com/ for therecipe",
+        "Then scrape the website https://www.allrecipes.com/ for the recipe",
         "Then search Youtube with description and give a summary",
     ],
     show_tool_calls=True,
     debug_mode=True,      
-    markdown=True,         
-    stream=True         
+    markdown=True,        
 )
 
-ai_recipe_agent.print_response(
-    "I have milk and butter, suggest me with some recipe."
-)
+ai_recipe_agent.print_response("I have milk and butter, suggest me with some recipe.", stream=True)

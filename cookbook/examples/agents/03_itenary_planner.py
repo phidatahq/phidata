8b386ba88608b5f70a78f532b86726183fdb1ca0
unnati914 <unnati002btit19@igdtuw.ac.in>
2024-12-05 17:52:14 +0530

    itenary planner

---
+++ cookbook/examples/agents/03_itenary_planner.py
@@ -0,0 +1,23 @@
+from phi.model.openai import OpenAIChat
+from phi.agent import Agent
+from phi.tools.exa import ExaTools
+from phi.tools.apify import ApifyTools
+
+agent = Agent(
+    name="Travel Buddy",
+    model=OpenAIChat(id="gpt-4o"),
+    tools=[ExaTools(), ApifyTools(web_scraper=True)],
+    show_tool_calls=True,
+    markdown=True,
+    debug_mode=True,
+    description="You are an itinerary planner agent. Your role is to assist users in creating detailed and personalized travel plans.",
+    instructions=[
+        "Crawl the website for itenary planning https://www.tripadvisor.in/Trips or https://www.makemytrip.com/",
+        "Make a concise itenary with flight options, residence options, tourist places, and the estimated budget",
+        "Look for user inputs and priortise it if they have specific demands",
+        "Do not give any numbers for reservations"
+    ],
+)
+
+agent.print_response("I want to go to Goa,with four members please make an itinerary")
+

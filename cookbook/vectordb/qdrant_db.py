8bbdbd35b3543d58891766aa5dcddc3e5faad6d9
lucifertrj <tarun.19ec144@cambridge.edu.in>
2024-12-07 00:09:01 +0530

    consistent with other vector db examples

--- cookbook/vectordb/qdrant_db.py
+++ cookbook/vectordb/qdrant_db.py
@@ -1,49 +1,30 @@
-# pip install phidata openai duckduckgo-search qdrant-client 
-# pip install bs4 sqlalchemy
-import os
-from phi.knowledge.website import WebsiteKnowledgeBase
+# pip install qdrant-client 
 from phi.vectordb.qdrant import Qdrant
-
 from phi.agent import Agent
-from phi.storage.agent.sqlite import SqlAgentStorage
-from phi.model.openai import OpenAIChat
-from phi.tools.duckduckgo import DuckDuckGo
+from phi.knowledge.pdf import PDFUrlKnowledgeBase
 
-# get Qdrant credentials from https://cloud.qdrant.io/
-QDRANT_URL = "<endpoint-url>"
-QDRANT_API_KEY = "<cluster-key>"
-OPENAI_API_KEY = "<replace-with-your-key>" #platform.openai.com
-
-os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
-
-# vector database => COllection name or Index name
-COLLECTION_NAME = "agentic-rag"
+# run qdrant client locally
+"""
+- Run the docker image: docker pull qdrant/qdrant
+- Then, run the service:
+docker run -p 6333:6333 -p 6334:6334 \
+    -v $(pwd)/qdrant_storage:/qdrant/storage:z \
+    qdrant/qdrant
+"""
+COLLECTION_NAME = "thai-recipes"
 
 vector_db = Qdrant(
     collection=COLLECTION_NAME,
-    url=QDRANT_URL,
-    api_key=QDRANT_API_KEY
+    url="http://localhost:6333"
 )
 
-URL = ["https://qdrant.tech/documentation/overview/"] # knowledge source
-
-knowledge_base = WebsiteKnowledgeBase(
-    urls = URL,
-    vector_db=vector_db, 
-    max_links=10
+knowledge_base = PDFUrlKnowledgeBase(
+    urls=["https://phi-public.s3.amazonaws.com/recipes/ThaiRecipes.pdf"],
+    vector_db=vector_db,
 )
 
-knowledge_base.load() # index
+knowledge_base.load(recreate=False)  # Comment out after first run
 
-agent = Agent(
-    model = OpenAIChat(id="gpt-4o"),
-    knowledge_base = knowledge_base, 
-    storage = SqlAgentStorage(table_name="agentic-rag",db_file="qdrant_docs.db"),
-    add_history_to_messages = True,
-    tools = [DuckDuckGo()], 
-    markdown=True,
-    show_tool_calls=True,
-)
-
-query = "what is todays MSFT stock price?"
-agent.print_response(query,stream=True)
+# Create and use the agent
+agent = Agent(knowledge_base=knowledge_base, use_tools=True, show_tool_calls=True)
+agent.print_response("List down the ingredients to make Massaman Gai", markdown=True)

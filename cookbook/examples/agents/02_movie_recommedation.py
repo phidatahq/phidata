718943b8c53c597c6edb524079ab30e069df7675
unnati914 <unnati002btit19@igdtuw.ac.in>
2024-12-06 12:36:42 +0530

    firecrawl

--- cookbook/examples/agents/02_movie_recommedation.py
+++ cookbook/examples/agents/02_movie_recommedation.py
@@ -1,24 +1,25 @@
 from phi.agent import Agent
 from phi.model.openai import OpenAIChat
-from phi.tools.apify import ApifyTools
+from phi.tools.firecrawl import FirecrawlTools
 from phi.tools.exa import ExaTools
 
 movie_recommedation_agent = Agent(
     name="PopcornPal",
     tools=[
         ExaTools(),
-        ApifyTools() 
+        FirecrawlTools(scrape=True, crawl=False), 
     ],
     model=OpenAIChat(id="gpt-4o"),
     description="You are a movie recommendation agent. You search movies by doing Exa search then scraping through a website.",
     instructions=[
-        "Crawl the website https://www.imdb.com/",
+        "Scrape the website https://www.themoviedb.org/ or https://www.imdb.com/",
         "Give a result with rating of the movie, it's genre, description, recommended age to watch,language, similar trailers like of recommended one and upcoming movies, best movies to watch for that particular year, remember to give the output in this format",
-        "Do not give the IMDB link to the movie",
+        "Do not give the link to the movie",
         "Use tables to give the result"
+        "You should give the upcoming trailers, that are expected to come after the ones you have recommended"
     ],
     show_tool_calls=True,
-    debug_mode=False,      
+    debug_mode=True,      
     markdown=True,
 )
-movie_recommedation_agent.print_response("Suggest some indian romantic movie", stream=True)
+movie_recommedation_agent.print_response("Suggest four latest indian romantic movie released in 2024", stream=True)

134a4730e98c3f8c96b1b6c37ff9cd5ff7b110e7
Ashpreet Bedi <ashpreet@phidata.com>
2024-12-07 13:00:05 +0000

    Ollama finance agent update

--- cookbook/providers/ollama/finance_agent.py
+++ cookbook/providers/ollama/finance_agent.py
@@ -8,10 +8,10 @@ agent = Agent(
     model=Ollama(id="llama3.1:8b"),
     tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True)],
     show_tool_calls=True,
-    description="You are an investment analyst that researches stocks and helps users make informed decisions.",
-    instructions=["Use tables to display data where possible."],
+    instructions="Use tables to display data",
     markdown=True,
 )
 
-# agent.print_response("Share the NVDA stock price and analyst recommendations", stream=True)
-agent.print_response("Summarize fundamentals for TSLA", stream=True)
+agent.print_response(
+    "Summarize and compare analyst recommendations and fundamentals for TSLA and NVDA. Show in tables.", stream=True
+)

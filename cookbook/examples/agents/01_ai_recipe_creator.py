1da0af0563f6bc0106af2f2a731969e1da1340b9
unnati914 <unnati002btit19@igdtuw.ac.in>
2024-12-09 00:19:58 +0530

    resolved comments

--- cookbook/examples/agents/01_ai_recipe_creator.py
+++ cookbook/examples/agents/01_ai_recipe_creator.py
@@ -1,27 +1,22 @@
-
 from phi.agent import Agent
-from phi.model.openai import OpenAIChat
-from phi.tools.youtube_tools import YouTubeTools
-from phi.tools.apify import ApifyTools
-from phi.tools.exa import ExaTools
+from phi.knowledge.pdf import PDFUrlKnowledgeBase
+from phi.vectordb.pgvector import PgVector
 
-ai_recipe_agent = Agent(
+db_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
+
+knowledge_base = PDFUrlKnowledgeBase(
+    urls=["https://www.poshantracker.in/pdf/Awareness/MilletsRecipeBook2023_Low%20Res_V5.pdf", "https://www.cardiff.ac.uk/__data/assets/pdf_file/0003/123681/Recipe-Book.pdf"],
+    vector_db=PgVector(table_name="recipes", db_url=db_url),
+)
+knowledge_base.load(recreate=False)
+
+recipe_agent = Agent(
     name="RecipeGenie",
-    tools=[
-        ExaTools(),
-        ApifyTools(web_scraper=True),
-        YouTubeTools()
-    ],
-    model=OpenAIChat(id="gpt-4o"),
-    description="You are a recipe suggestor. Suggest recipes based on input ingredients by doing exa Search and summarizing from YouTube videos.",
-    instructions=[
-        "Search exa for recipes",
-        "Then scrape the website https://www.allrecipes.com/ for the recipe",
-        "Then search Youtube with description and give a summary",
-    ],
-    show_tool_calls=True,
-    debug_mode=True,      
-    markdown=True,        
+    knowledge_base=knowledge_base,
+    search_knowledge=True,
+    instructions=["Search for recipes based on the ingredients and time available from the knowledge base.",
+                  "Additionally, include the nutritional information and cooking instructions for the recommended recipes.",
+                  "You can alternatively search Youtube for video links related to the recommended recipes (as given from the knowledge base) from famous chefs with most subscribers and include them in the response."],
 )
 
-ai_recipe_agent.print_response("I have milk and butter, suggest me with some recipe.", stream=True)
+recipe_agent.print_response("I have milk and butter, please suggest me a recipe with 500g protein", markdown=True)

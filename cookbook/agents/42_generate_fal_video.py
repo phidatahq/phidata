7cf736dd859be8ad9169cd2e794f58b0de4e2f9b
anuragts <anuragsharma011011@gmail.com>
2024-12-09 22:17:00 +0530

    fix: improve instructions

--- cookbook/agents/42_generate_fal_video.py
+++ cookbook/agents/42_generate_fal_video.py
@@ -11,7 +11,6 @@ fal_agent = Agent(
         "When the user asks you to create a video, use the `run` tool to create the video.",
         "Return the URL as raw to the user.",
         "Don't convert video URL to markdown or anything else.",
-        "Use `fal-ai/hunyuan-video` model by default.",
         "Also pass the type of model of the tool, it can be either `image` or `video`.",
     ],
     markdown=True,

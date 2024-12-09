5b3c1508919d9368f6a9933e6d65f7ae83c3ad68
unnati914 <unnati002btit19@igdtuw.ac.in>
2024-12-09 13:02:11 +0530

    study planner

---
+++ cookbook/examples/agents/study_partner.py
@@ -0,0 +1,22 @@
+from phi.agent import Agent
+from phi.model.openai import OpenAIChat
+from phi.tools.youtube_tools import YouTubeTools
+from phi.tools.exa import ExaTools
+
+study_partner = Agent(
+    name="StudyScount",
+    model=OpenAIChat(id="gpt-4o"),
+    tools=[ExaTools(), YouTubeTools()],
+    markdown=False,
+    debug_mode=True,
+    description="You are a study partner who assists users in finding resources, answering questions, and providing explanations on various topics.",
+    instructions=[
+        "Use Exa to search for relevant information on the given topic.",
+        "Provide detailed explanations, examples,articles, PDF document links from Medium, communities to join for collaborating and additional resources to help the student understand the concept better.",
+        "Give all https:// links which are clickable directly in the response and not in a markdown format and ensure all the links are updated and working.",
+        "Include links to educational videos, roadmaps from YouTube that explain the topic in an engaging and informative manner.",
+        "Ensure that the information shared is accurate, up-to-date, and relevant to the user's query.",
+        "Provide day-to-day study planner,project ideas, motivation, and guidance to help students stay focused and productive.",
+    ],
+)
+study_partner.print_response("I struggle a lot with Postgres. Please share some resources.", stream=True)

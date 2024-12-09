3243c5b15147a7e2599f2acba2fba0464cc56afe
anuragts <anuragsharma011011@gmail.com>
2024-12-09 21:39:03 +0530

    fix: move to fal tools

--- cookbook/agents/42_generate_fal_video.py
+++ cookbook/agents/42_generate_fal_video.py
@@ -1,16 +1,17 @@
 from phi.agent import Agent
 from phi.model.openai import OpenAIChat
-from phi.tools.fal import Fal
+from phi.tools.fal_tools import FalTools
 
 fal_agent = Agent(
     model=OpenAIChat(id="gpt-4o"),
-    tools=[Fal()],
-    description="You are an AI agent that can generate videos using the 'fal' API.",
+    tools=[FalTools()],
+    description="You are an AI agent that can generate videos using the Fal API.",
     instructions=[
-        "When the user asks you to create a video, execute `run` on the tool to create the video.",
+        "When the user asks you to create a video, use the `run` tool to create the video.",
         "Return the URL as raw to the user.",
         "Don't convert video URL to markdown or anything else.",
         "Use `fal-ai/hunyuan-video` model by default.",
+        "Also pass the type of model of the tool, it can be either `image` or `video`.",
     ],
     markdown=True,
     debug_mode=True,
--- phi/tools/fal_tools.py
+++ phi/tools/fal_tools.py
@@ -8,6 +8,7 @@ from typing import Optional
 from phi.agent import Agent
 from phi.tools import Toolkit
 from phi.utils.log import logger
+from enum import Enum
 
 try:
     import fal_client  # type: ignore
@@ -15,46 +16,59 @@ except ImportError:
     raise ImportError("`fal_client` not installed. Please install using `pip install fal-client`")
 
 
-class Fal(Toolkit):
+class ModelType(Enum):
+    IMAGE = "image"
+    VIDEO = "video"
+    TEXT = "text"
+
+
+class FalTools(Toolkit):
     def __init__(
         self,
         api_key: Optional[str] = None,
         model: str = "fal-ai/hunyuan-video",
+        type: ModelType = ModelType.VIDEO,
     ):
         super().__init__(name="fal")
 
         self.api_key = api_key or getenv("FAL_API_KEY")
+        self.model = model
+        self.type = type
         if not self.api_key:
             logger.error("FAL_API_KEY not set. Please set the FAL_API_KEY environment variable.")
-
+        self.seen_logs: set[str] = set()
         self.register(self.run)
 
     def on_queue_update(self, update):
         if isinstance(update, fal_client.InProgress) and update.logs:
             for log in update.logs:
-                logger.info(log["message"])
+                message = log["message"]
+                if message not in self.seen_logs:
+                    logger.info(message)
+                    self.seen_logs.add(message)
 
-    def run(self, agent: Agent, prompt: str, model: str) -> str:
+    def run(self, agent: Agent, prompt: str, model: Optional[str] = None, type: Optional[ModelType] = None) -> str:
         """
         Use this function to run a model with a given prompt.
 
         Args:
             prompt (str): A text description of the task.
             model (str): The model to use.
+            type (ModelType): The type of the model to use. It can be either `image` or `video` or `text`.
         Returns:
             str: Return the result of the model.
         """
         try:
             result = fal_client.subscribe(
-                model,
+                model or self.model,
                 arguments={"prompt": prompt},
                 with_logs=True,
                 on_queue_update=self.on_queue_update,
             )
-            if video_url := result.get("video", {}).get("url", ""):
-                return video_url
-            elif image_url := result.get("image", {}).get("url", ""):
-                return image_url
+            if type == ModelType.VIDEO:
+                return result.get("video", {}).get("url", "")
+            elif type == ModelType.IMAGE:
+                return result.get("image", {}).get("url", "")
             else:
                 return str(result)
         except Exception as e:

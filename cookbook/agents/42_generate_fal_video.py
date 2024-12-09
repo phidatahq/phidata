d9951ab57e237303a73fd060e77db101f53dee4c
anuragts <anuragsharma011011@gmail.com>
2024-12-09 18:41:51 +0530

    fix: create a fal tool

---
+++ cookbook/agents/42_generate_fal_video.py
@@ -0,0 +1,20 @@
+from phi.agent import Agent
+from phi.model.openai import OpenAIChat
+from phi.tools.fal import Fal
+
+fal_agent = Agent(
+    model=OpenAIChat(id="gpt-4o"),
+    tools=[Fal()],
+    description="You are an AI agent that can generate videos using the Fal API.",
+    instructions=[
+        "When the user asks you to create a video, use the `run` tool to create the video.",
+        "Return the URL as raw to the user.",
+        "Don't convert video URL to markdown or anything else.",
+        "Use `fal-ai/hunyuan-video` model by default.",
+    ],
+    markdown=True,
+    debug_mode=True,
+    show_tool_calls=True,
+)
+
+fal_agent.print_response("Generate video of ballon in the ocean")
--- phi/tools/fal.py
+++ phi/tools/fal.py
@@ -15,43 +15,49 @@ except ImportError:
     raise ImportError("`fal_client` not installed. Please install using `pip install fal-client`")
 
 
-class HunyuanVideo(Toolkit):
+class Fal(Toolkit):
     def __init__(
         self,
         api_key: Optional[str] = None,
+        model: str = "fal-ai/hunyuan-video",
     ):
-        super().__init__(name="hunyuan_video")
+        super().__init__(name="fal")
 
         self.api_key = api_key or getenv("FAL_API_KEY")
         if not self.api_key:
             logger.error("FAL_API_KEY not set. Please set the FAL_API_KEY environment variable.")
 
-        self.register(self.generate_video)
+        self.register(self.run)
 
     def on_queue_update(self, update):
         if isinstance(update, fal_client.InProgress) and update.logs:
             for log in update.logs:
                 logger.info(log["message"])
 
-    def generate_video(self, agent: Agent, prompt: str) -> str:
+    def run(self, agent: Agent, prompt: str, model: str) -> str:
         """
-        Use this function to generate a video given a prompt.
+        Use this function to run a model with a given prompt.
 
         Args:
-            prompt (str): A text description of the desired video.
-
+            prompt (str): A text description of the task.
+            model (str): The model to use.
         Returns:
-            str: Generated video URL.
+            str: Return the result of the model.
         """
         try:
             result = fal_client.subscribe(
-                "fal-ai/hunyuan-video",
+                model,
                 arguments={"prompt": prompt},
                 with_logs=True,
                 on_queue_update=self.on_queue_update,
             )
-            video_url = result.get("video", {}).get("url", "")
-            return video_url
+            if video_url := result.get("video", {}).get("url", ""):
+                return video_url
+            elif image_url := result.get("image", {}).get("url", ""):
+                print(image_url)
+                return image_url
+            else:
+                return str(result)
         except Exception as e:
-            logger.error(f"Failed to generate video: {e}")
+            logger.error(f"Failed to run model: {e}")
             return f"Error: {e}"

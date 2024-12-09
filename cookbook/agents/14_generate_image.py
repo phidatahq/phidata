1d7808fedd6ec3ad3784897eb93fb7f3a0e86c57
Ashpreet Bedi <ashpreet@phidata.com>
2024-12-09 17:07:08 +0000

    Update image generation

--- cookbook/agents/14_generate_image.py
+++ cookbook/agents/14_generate_image.py
@@ -1,21 +1,24 @@
 from phi.agent import Agent
-from phi.model.openai import OpenAIChat
 from phi.tools.dalle import Dalle
+from phi.model.openai import OpenAIChat
 
 image_agent = Agent(
     model=OpenAIChat(id="gpt-4o"),
     tools=[Dalle()],
     description="You are an AI agent that can generate images using DALL-E.",
-    instructions=[
-        "When the user asks you to create an image, use the `create_image` tool to create the image.",
-        "The image will be displayed in the UI automatically below your response, so you don't need to show the image URL in your response.",
-        "Politely and courteously let the user know that the image has been generated and will be displayed below as soon as its ready.",
-    ],
+    instructions="When the user asks you to create an image, use the `create_image` tool to create the image.",
     markdown=True,
-    debug_mode=True,
     show_tool_calls=True,
 )
 
 image_agent.print_response("Generate an image of a white siamese cat")
-# print(image_agent.run_response.images)
-# print(image_agent.get_images())
+
+images = image_agent.get_images()
+if images and isinstance(images, list):
+    for image_response in images:
+        image_data = image_response.get("data")  # type: ignore
+        if image_data:
+            for image in image_data:
+                image_url = image.get("url")  # type: ignore
+                if image_url:
+                    print(image_url)

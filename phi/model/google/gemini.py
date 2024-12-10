0e98701b58b3fcccfcb8f34f3b20a6d0b3786d62
Dirk Brand <dirk@volter.ai>
2024-12-10 18:17:34 +0200

    Fix gemini reference

--- phi/model/google/gemini.py
+++ phi/model/google/gemini.py
@@ -267,7 +267,7 @@ class Gemini(Model):
         Returns:
             GenerateContentResponse: The response from the model.
         """
-        return self.get_client().generate_media(contents=self._format_messages(messages))
+        return self.get_client().generate_content(contents=self._format_messages(messages))
 
     def invoke_stream(self, messages: List[Message]):
         """
@@ -279,7 +279,7 @@ class Gemini(Model):
         Returns:
             Iterator[GenerateContentResponse]: The response from the model as a stream.
         """
-        yield from self.get_client().generate_media(
+        yield from self.get_client().generate_content(
             contents=self._format_messages(messages),
             stream=True,
         )

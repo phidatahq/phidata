15e5575af3dfdae9801dbf10b812ff719ae77274
Manthan Gupta <manthangupta109@gmail.com>
2024-12-10 19:17:25 +0530

    fix prompts and instructions

--- cookbook/examples/agents/01_ai_recipe_creator.py
+++ cookbook/examples/agents/01_ai_recipe_creator.py
@@ -6,7 +6,10 @@ from phi.tools.exa import ExaTools
 db_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
 
 knowledge_base = PDFUrlKnowledgeBase(
-    urls=["https://www.poshantracker.in/pdf/Awareness/MilletsRecipeBook2023_Low%20Res_V5.pdf", "https://www.cardiff.ac.uk/__data/assets/pdf_file/0003/123681/Recipe-Book.pdf"],
+    urls=[
+        "https://www.poshantracker.in/pdf/Awareness/MilletsRecipeBook2023_Low%20Res_V5.pdf",
+        "https://www.cardiff.ac.uk/__data/assets/pdf_file/0003/123681/Recipe-Book.pdf",
+    ],
     vector_db=PgVector(table_name="recipes", db_url=db_url),
 )
 knowledge_base.load(recreate=False)
@@ -17,10 +20,14 @@ recipe_agent = Agent(
     search_knowledge=True,
     tools=[ExaTools()],
     markdown=True,
-    instructions=["Search for recipes based on the ingredients and time available from the knowledge base.",
-                  "Include the nutritional information, prep time, cooking instructions for the recommended recipes.",
-                  "Always search exa for recipe links or tips related to the recipes apart from knowledge base.",
-                  "Provide a list of recipes that match the user's requirements and preferences."],
+    instructions=[
+        "Search for recipes based on the ingredients and time available from the knowledge base.",
+        "Include the exact calories, preparation time, cooking instructions, and highlight allergens for the recommended recipes.",
+        "Always search exa for recipe links or tips related to the recipes apart from knowledge base.",
+        "Provide a list of recipes that match the user's requirements and preferences.",
+    ],
 )
 
-recipe_agent.print_response("I have spinach and brocolli, please suggest me a recipe for 10g protein ", stream=True)
+recipe_agent.print_response(
+    "I have potatoes, tomatoes, onions, garlic, ginger, and chicken. Suggest me a quick recipe for dinner", stream=True
+)

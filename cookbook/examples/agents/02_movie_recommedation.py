94dfc1890be9603bbe8514dfdcd077e581113477
Manthan Gupta <manthangupta109@gmail.com>
2024-12-10 16:59:14 +0530

    fix prompt

--- cookbook/examples/agents/02_movie_recommedation.py
+++ cookbook/examples/agents/02_movie_recommedation.py
@@ -5,7 +5,7 @@ from phi.tools.exa import ExaTools
 movie_recommendation_agent = Agent(
     name="PopcornPal",
     tools=[
-        ExaTools(), 
+        ExaTools(),
     ],
     model=OpenAIChat(id="gpt-4o"),
     description=(
@@ -18,12 +18,12 @@ movie_recommendation_agent = Agent(
         "Include trailers for movies similar to the recommendations and upcoming movies of the same genre or from related directors/actors.",
         "Give atleast 5 movie recommendations for each query",
         "Present the output in a well-structured markdown table for readability.",
-        "Ensure all movie data is correct, especially for recent or upcoming releases."
+        "Ensure all movie data is correct, especially for recent or upcoming releases.",
     ],
     markdown=True,
 )
 
 movie_recommendation_agent.print_response(
-    "Suggest some thriller movies to watch.",
-    stream=True
+    "Suggest some thriller movies to watch with a rating of 8 or above on IMDB. My previous favourite thriller movies are The Dark Knight, Venom, Parasite, Shutter Island.",
+    stream=True,
 )

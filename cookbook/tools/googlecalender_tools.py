57b62b8c1178547f3d9c58ea8b426f7ac58ad1d5
Vedant Sasane <vedant.sasane@wolkus.com>
2024-12-17 10:26:17 +0530

    Review Comments Resolved

--- cookbook/tools/googlecalender_tools.py
+++ cookbook/tools/googlecalender_tools.py
@@ -6,20 +6,18 @@ import os
 
 try:
     from tzlocal import get_localzone_name
-except ImportError:
+except (ModuleNotFoundError, ImportError):
     raise ImportError("`tzlocal not found` install using `pip install tzlocal`")
-
-GOOGLE_CALENDER_INSTRUCTION = f"""
-You are scheduing assistant . Today is {datetime.datetime.now()} and Users timezone is {get_localzone_name()}.
-You should help users to perform these actions in their google calender :
-    - get their scheduled events from a certain date and time
-    - create events based on provided details
-"""
 
 agent = Agent(
     tools=[GoogleCalenderTools(credentials_path="<PATH_TO_YOUR_CREDENTIALS_FILE>")],
     show_tool_calls=True,
-    instructions=[GOOGLE_CALENDER_INSTRUCTION],
+    instructions=[f"""
+You are scheduling assistant . Today is {datetime.datetime.now()} and the users timezone is {get_localzone_name()}.
+You should help users to perform these actions in their Google calendar :
+    - get their scheduled events from a certain date and time
+    - create events based on provided details
+"""],
     provider=MistralChat(api_key=os.getenv("MISTRAL_API_KEY")),
     add_datetime_to_instructions=True,
 )

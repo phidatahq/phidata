e4113a53c3f8cf9ffcf135e868a04f9f28e6f469
Dirk Brand <51947788+dirkbrnd@users.noreply.github.com>
2024-12-19 14:41:05 +0200

    Make confluence cookbook more clear (#1607)
    ## Description
    
    Makes some clarifications on the confluence cookbook to make it more
    reliable

--- cookbook/tools/confluence_tools.py
+++ cookbook/tools/confluence_tools.py
@@ -16,7 +16,7 @@ agent.print_response("How many spaces are there and what are their names?")
 agent.print_response("What is the content present in page 'Large language model in LLM space'")
 
 ## getting page details in a particular space
-agent.print_response("Can you extract all the page names from 'LLM space'")
+agent.print_response("Can you extract all the page names from 'LLM' space")
 
 ## creating a new page in a space
-agent.print_response("Can you create a new page named 'TESTING' in 'LLM space'")
+agent.print_response("Can you create a new page named 'TESTING' in 'LLM' space")

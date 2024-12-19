304690590603fda245c3e34ba0050ce728027df1
Shivam singh <112329366+Shivam909058@users.noreply.github.com>
2024-12-19 18:40:42 +0530

    Update 07_monitoring.py

--- cookbook/agents/07_monitoring.py
+++ cookbook/agents/07_monitoring.py
@@ -13,17 +13,7 @@ from phi.model.deepseek import DeepSeekChat
 # export DEEPSEEK_API_KEY="your-api-key-here"
 #
 # NOTE: After setting the API key, restart your terminal or environment to apply the changes  by shuting down the terminal and run the script again. 
+from phi.agent import Agent
 
-# Initialize the agent with the DeepSeekChat model
-agent = Agent(model=DeepSeekChat(), markdown=True)
-
-# Example 1: Get the response in a variable
-# Uncomment the following lines to use:
-# run: RunResponse = agent.run("Share a 2 sentence horror story.")
-# print(run.content)
-
-# Example 2: Print the response in the terminal
-print("Printing the response in the terminal")
-agent.print_response(
-    "Give me a code for a React app which says 'Hello World' and provide all file code"
-)
+agent = Agent(markdown=True, monitoring=True)
+agent.print_response("Share a 2 sentence horror story")

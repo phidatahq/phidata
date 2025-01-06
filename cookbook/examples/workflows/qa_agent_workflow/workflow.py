16b509d976a0ae89dcef9370b3e5923b75cda9fe
Antaripa Saha <antaripasaha2000@gmail.com>
2025-01-06 15:17:25 +0530

    workflow for qa_agent updated

--- cookbook/examples/workflows/qa_agent_workflow/workflow.py
+++ cookbook/examples/workflows/qa_agent_workflow/workflow.py
@@ -1,4 +1,6 @@
 import logging
+import os
+import time
 from phi.run.response import RunResponse
 from phi.workflow import Workflow
 from phi.agent import Agent
@@ -8,9 +10,12 @@ from phi.vectordb.lancedb import LanceDb
 from phi.vectordb.search import SearchType
 from phi.embedder.openai import OpenAIEmbedder
 from phi.document.chunking.recursive import RecursiveChunking
+from typing import List
 from dotenv import load_dotenv
 
 load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -22,49 +27,52 @@ class QAWorkflow(Workflow):
     QA Workflow to scrape websites, chunk data, store it in a vector database, and answer a question.
     args: website_urls (list): List of website URLs to scrape and process.
     """
-    def __init__(self, website_urls, **data):
-        super().__init__(**data)
-        self.website_urls = website_urls
+    website_urls: List = [
+        "https://lilianweng.github.io/posts/2023-06-23-agent/",
+        "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
+        "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
+    ]
+    knowledge: WebsiteKnowledgeBase = WebsiteKnowledgeBase(
+        urls=website_urls,
+        max_links=10,
+        vector_db=LanceDb(
+            table_name="qa_agent_workflow",
+            uri="/tmp/lancedb",
+            search_type=SearchType.vector,
+            embedder=OpenAIEmbedder(model="text-embedding-ada-002"),
+        ),
+        chunking_strategy=RecursiveChunking()
+    )
 
-        self.qa_agent: Agent = Agent(
-            model=OpenAIChat(id="gpt-4o"),
-            description="You are a helpful assistant that can answer questions for a given question from the knowledge base.",
-            instructions=[
-                "Use the following pieces of retrieved context to answer the question.",
-                "Your goal is to answer the user's question in detail.",
-                "Provide a well-structured, clear, and concise answer.",
-            ],
-            knowledge=WebsiteKnowledgeBase(
-                urls=self.website_urls,  # Use instance-specific attribute
-                max_links=10,  # Follow up to 10 links from each website
-                vector_db=LanceDb(
-                    table_name="qa_agent_workflow",
-                    uri="/tmp/lancedb2",
-                    search_type=SearchType.vector,
-                    embedder=OpenAIEmbedder(model="text-embedding-ada-002"),
-                ),
-                chunking_strategy=RecursiveChunking()
-            ),
-            search_knowledge=True,
-            show_tool_calls=True,
-            debug_mode=True,
-        )
+    qa_agent: Agent = Agent(
+        model=OpenAIChat(id="gpt-4o"),
+        description="You are a helpful assistant that can answer questions for a given question from the knowledge base.",
+        instructions=[
+            "Use the following pieces of retrieved context to answer the question.",
+            "Your goal is to answer the user's question in detail.",
+            "Provide a well-structured, clear, and concise answer.",
+        ],
+        knowledge=knowledge,
+        search_knowledge=True,
+        show_tool_calls=True,
+        debug_mode=True,
+    )
 
     def load_knowledge_base(self, recreate=False):
         """
         Loads the scraped and chunked content into the knowledge base.
         """
         logger.info("Loading knowledge base...")
-        self.qa_agent.knowledge_base.load(recreate=recreate)
+        self.qa_agent.knowledge.load(recreate=recreate)
         logger.info("Knowledge base loaded successfully.")
 
     def generate_answer(self, question):
         answer: RunResponse = RunResponse(content=None)
         logger.info(f"Generating answer for {question}:\n")
         answer = self.qa_agent.run(question)
-        return answer.content
+        return RunResponse(content=answer.content)
 
-    def run(self, question):
+    def run(self, question, knowledge_base_recreate=True):
         """
         Runs the workflow: scrapes the websites, chunks the content, stores it in the vector database,
         and answers the given question.
@@ -73,24 +81,32 @@ class QAWorkflow(Workflow):
             question (str): The question to ask the QA Agent.
         """
 
-        self.load_knowledge_base(recreate=True)
+        load_start = time.time()
+        self.load_knowledge_base(recreate=knowledge_base_recreate)
+        load_end = time.time()
+        duration_load = load_end - load_start
+        logger.info(f"Loading of the website done in {duration_load:.2f} seconds")
         # Generate Answer from the QA Agent
         logger.info(f"Asking question: {question}")
+        ans_start = time.time()
         response = self.generate_answer(question)
+        ans_end = time.time()
+        duration_ans = ans_end - ans_start
+        logger.info(f"Answer generated in {duration_ans:.2f} seconds")
 
         return response
 
 
 # Entry Point for the Workflow
 if __name__ == "__main__":
-    website_urls = [
-        "https://lilianweng.github.io/posts/2023-06-23-agent/",
-        "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
-        "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
-    ]
+    flow_start = time.time()
 
-    question = "What does Lilian Weng say about the types of agent memory?"
-
+    question = "How is the importance score of each word measured given a classifier?"
     # Run the QA Workflow
-    qa_workflow = QAWorkflow(website_urls)
-    qa_workflow.run(question)
+    qa_workflow = QAWorkflow()
+    qa_response = qa_workflow.run(question=question, knowledge_base_recreate=False)
+    print(qa_response.content)
+    flow_end = time.time()
+    duration_flow = flow_end - flow_start
+    logger.info(f"The question generation workflow is completed in {duration_flow:.2f} seconds")
+

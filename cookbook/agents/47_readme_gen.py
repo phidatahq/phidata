84f22868e23ea941c1f8f059a7687997c96aedd2
anuragts <anuragsharma011011@gmail.com>
2025-01-06 13:16:24 +0530

    feat: write file to local

--- cookbook/agents/47_readme_gen.py
+++ cookbook/agents/47_readme_gen.py
@@ -1,11 +1,12 @@
 from phi.agent import Agent
 from phi.model.openai import OpenAIChat
 from phi.tools.github import GithubTools
+from phi.tools.write_to_local_tools import WriteToLocal
 
 readme_gen_agent = Agent(
     model=OpenAIChat(id="gpt-4o"),
     name="Readme Generator Agent",
-    tools=[GithubTools()],
+    tools=[GithubTools(), WriteToLocal()],
     markdown=True,
     debug_mode=True,
     instructions=[
@@ -14,7 +15,10 @@ readme_gen_agent = Agent(
         "You'll use the `get_repository` tool to get the repository details."
         "You have to pass the repo_name as argument to the tool. It should be in the format of owner/repo_name. If given url extract owner/repo_name from it."
         "Also call the `get_repository_languages` tool to get the languages used in the repository."
-        "For now only print the response as given don't convert it to md",
+        "You are an AI assistant that can write Readme files for open-source GitHub projects."
+        "Return your response in markdown format. You will be provided with languages used is project, the repo URL, clone URL etc.Give an good response with like how to clone the project and install the dependency , run the project etc. Also give icons like of mit license , size of repo etc. "
+        "Ignore the lines code in each language but include the languages in other sections with their icons",
+        "You have to write the readme file in the current directory",
     ],
 )
 

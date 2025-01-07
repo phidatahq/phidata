2f3350576e06767c9a9793d3347b1ccb6e8d9a87
Vedant Sasane <vedant.sasane@wolkus.com>
2025-01-07 22:25:20 +0530

    Steps For Creating OAuth Credentials Added in Cookbook

--- cookbook/tools/googlecalendar_tools.py
+++ cookbook/tools/googlecalendar_tools.py
@@ -4,6 +4,48 @@ from phi.model.mistral import MistralChat
 import datetime
 import os
 
+"""
+Steps to get the Google OAuth Credentials (Reference : https://developers.google.com/calendar/api/quickstart/python)
+
+1. Enable Google Calender API 
+    - Go To https://console.cloud.google.com/apis/enableflow?apiid=calendar-json.googleapis.com
+    - Select Project and Enable The API
+
+2. Go To API & Service -> OAuth Consent Screen
+
+3.Select User Type . If you're google workspace user select Internal else Select External
+    - If you are Google Workspace User select Internal
+    - Else Select External
+
+4.Fill in the app details (App name, logo, support email, etc.).
+
+5. Select Scope
+    - Click on Add or Remove Scope
+    - Search for Google Calender API (Make Sure you've enabled Google calender API otherwise scopes wont be visible)
+    - Select Scopes Accordingly
+        - From the dropdown check on /auth/calendar scope
+    - Save and Continue
+
+
+6. Adding Test User 
+    - Click Add Users and enter the email addresses of the users you want to allow during testing.
+    - NOTE : Only these users can access the app's OAuth functionality when the app is in "Testing" mode.
+    If anyone else tries to authenticate, they'll see an error like: "Error 403: access_denied."
+    - To make the app available to all users, you'll need to move the app's status to "In Production.".
+    Before doing so, ensure the app is fully verified by Google if it uses sensitive or restricted scopes.
+    - Click on Go back to Dashboard
+
+
+7. Generate OAuth 2.0 Client ID
+    - Go To Credentials 
+    - Click on Create Credentials -> OAuth Client ID
+    - Select Application Type as Desktop app
+    - Download JSON
+    - Provide the Path of credentials to Google Calender tool
+
+"""
+
+
 try:
     from tzlocal import get_localzone_name
 except (ModuleNotFoundError, ImportError):

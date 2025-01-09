902aa751f6bec68f8b4675309cb2e188b7025261
Ashpreet <ashpreetbedi@users.noreply.github.com>
2025-01-09 22:27:57 +0000

    Accuracy Eval

--- cookbook/agents_101/01_basic.py
+++ cookbook/agents_101/01_basic.py
@@ -3,5 +3,4 @@ from agno.models.openai import OpenAIChat
 
 agent = Agent(model=OpenAIChat(id="gpt-4o"), instructions="Speak like a Shakespearean bard", markdown=True)
 
-print(agent.model)
-# agent.print_response("Share a 2 sentence story", stream=True)
+agent.print_response("Share a 2 sentence story", stream=True)
--- evals/accuracy/openai/calculator.py
+++ evals/accuracy/openai/calculator.py
@@ -1,13 +1,15 @@
 from typing import Optional
 
+from rich.pretty import pprint
+
 from agno.agent import Agent
-from agno.eval import Eval, EvalResult
+from agno.eval.accuracy import AccuracyEval, AccuracyResult
 from agno.models.openai import OpenAIChat
 from agno.tools.calculator import Calculator
 
 
 def multiply_and_exponentiate():
-    evaluation = Eval(
+    evaluation = AccuracyEval(
         agent=Agent(
             model=OpenAIChat(id="gpt-4o-mini"),
             tools=[Calculator(add=True, multiply=True, exponentiate=True)],
@@ -15,25 +17,27 @@ def multiply_and_exponentiate():
         question="What is 10*5 then to the power of 2? do it step by step",
         expected_answer="2500",
     )
-    result: Optional[EvalResult] = evaluation.print_result()
+    result: Optional[AccuracyResult] = evaluation.run(print_results=True)
+    pprint(result)
+    # result: Optional[EvalResult] = evaluation.print_result()
 
-    assert result is not None and result.accuracy_score >= 8
+    # assert result is not None and result.accuracy_score >= 8
 
 
-def factorial():
-    evaluation = Eval(
-        agent=Agent(
-            model=OpenAIChat(id="gpt-4o-mini"),
-            tools=[Calculator(factorial=True)],
-        ),
-        question="What is 10!?",
-        expected_answer="3628800",
-    )
-    result: Optional[EvalResult] = evaluation.print_result()
+# def factorial():
+#     evaluation = Eval(
+#         agent=Agent(
+#             model=OpenAIChat(id="gpt-4o-mini"),
+#             tools=[Calculator(factorial=True)],
+#         ),
+#         question="What is 10!?",
+#         expected_answer="3628800",
+#     )
+#     result: Optional[EvalResult] = evaluation.print_result()
 
-    assert result is not None and result.accuracy_score >= 8
+#     assert result is not None and result.accuracy_score >= 8
 
 
 if __name__ == "__main__":
     multiply_and_exponentiate()
-    factorial()
+    # factorial()

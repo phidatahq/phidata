c764d81272f65ffd06479c4673dd4ae292ae00f9
Manthan Gupta <manthangupta109@gmail.com>
2025-01-10 19:27:42 +0530

    fix

--- cookbook/knowledge/youtube_kb.py
+++ cookbook/knowledge/youtube_kb.py
@@ -8,18 +8,12 @@ from agno.vectordb.pgvector.pgvector import PgVector
 api_key = getenv("QDRANT_API_KEY")
 qdrant_url = getenv("QDRANT_URL")
 
-# vector_db = Qdrant(collection="youtube-phidata", url=qdrant_url, api_key=api_key)
-
-# os.environ["OPENAI_API_KEY"] = "<replace-with-openai-key>"
-db_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
+vector_db = Qdrant(collection="youtube-phidata", url=qdrant_url, api_key=api_key)
 
 # Create a knowledge base with the PDFs from the data/pdfs directory
 knowledge_base = YouTubeKnowledgeBase(
     urls=["https://www.youtube.com/watch?v=CDC3GOuJyZ0"],
-    vector_db=PgVector(
-        table_name="youtube_documents",
-        db_url=db_url,
-    ),
+    vector_db=vector_db,
     reader=YouTubeReader(chunk=True),
 )
 knowledge_base.load(recreate=False)  # only once, comment it out after first run

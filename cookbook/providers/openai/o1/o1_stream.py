3eb14ad7f869010426bbee4a7f9b21541e361558
Dirk Brand <dirkbrnd@gmail.com>
2025-01-15 12:34:38 +0200

    Update

--- cookbook/providers/openai/o1/o1_stream.py
+++ cookbook/providers/openai/o1/o1_stream.py
@@ -2,6 +2,7 @@ from typing import Iterator  # noqa
 from phi.agent import Agent, RunResponse  # noqa
 from phi.model.openai import OpenAIChat
 
+# This will only work if you have access to the o1 model from OpenAI
 agent = Agent(model=OpenAIChat(id="o1"))
 
 # Print the response in the terminal

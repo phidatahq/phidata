e259eee21d8ad66f603afb0650719697ed62f3ab
Dirk Brand <dirkbrnd@gmail.com>
2025-01-17 15:48:11 +0200

    Update

--- cookbook/models/azure/openai/knowledge.py
+++ cookbook/models/azure/openai/knowledge.py
@@ -21,7 +21,7 @@ knowledge_base.load(recreate=False)  # Comment out after first run
 agent = Agent(
     model=AzureOpenAI(id="gpt-4o"),
     knowledge=knowledge_base,
-    use_tools=True,
     show_tool_calls=True,
+    debug_mode=True
 )
 agent.print_response("How to make Thai curry?", markdown=True)

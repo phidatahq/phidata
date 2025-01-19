affa3ceff4ce2a708e01a66593dd96bda97bec05
Türkalp Burak Kayrancıoğlu <bkayranci@gmail.com>
2025-01-19 15:25:09 +0300

    describe how to get token for telegram bot

--- cookbook/tools/telegram_tools.py
+++ cookbook/tools/telegram_tools.py
@@ -1,6 +1,12 @@
 from phi.agent import Agent
 from phi.tools.telegram import TelegramTools
 
+# How to get the token and chat_id:
+# 1. Create a new bot with BotFather on Telegram. https://core.telegram.org/bots/features#creating-a-new-bot
+# 2. Get the token from BotFather.
+# 3. Send a message to the bot.
+# 4. Get the chat_id by going to the URL:
+#    https://api.telegram.org/bot<your-bot-token>/getUpdates
 
 telegram_token = "<enter-your-bot-token>"
 chat_id = "<enter-your-chat-id>"

761a29c37053d9f76fc4c98e0fa6bacd3a262b95
Dirk Brand <dirkbrnd@gmail.com>
2025-01-20 05:59:51 +0200

    Update

--- libs/agno/agno/agent/agent.py
+++ libs/agno/agno/agent/agent.py
@@ -43,7 +43,7 @@ from agno.utils.safe_formatter import SafeFormatter
 from agno.utils.timer import Timer
 
 
-@dataclass(init=False, slots=True)
+@dataclass(init=False, slots=True)  # type: ignore
 class Agent:
     # --- Agent settings ---
     # Model for this Agent

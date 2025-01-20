ed3a2ebd7ba4cf318662aa7c533ecb0d9abfa5ca
Willem Carel de Jongh <willemcarel@gmail.com>
2025-01-20 14:54:13 +0200

    Refactor/sort cookbooks (#1830)
    ## Description
    Fix unsorted cookbooks
    Mongo DB setup script
    Small instruction update to clickhouse
    Fixes # (issue)
    
    ## Type of change
    
    Please check the options that are relevant:
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Model update
    - [ ] Infrastructure change
    
    ## Checklist
    
    - [ ] My code follows Phidata's style guidelines and best practices
    - [ ] I have performed a self-review of my code
    - [ ] I have added docstrings and comments for complex logic
    - [ ] My changes generate no new warnings or errors
    - [ ] I have added cookbook examples for my new addition (if needed)
    - [ ] I have updated requirements.txt/pyproject.toml (if needed)
    - [ ] I have verified my changes in a clean environment
    
    ## Additional Notes
    
    Include any deployment notes, performance implications, or other
    relevant information:

--- cookbook/scripts/run_mongodb.sh
+++ cookbook/scripts/run_mongodb.sh
@@ -1,1 +1,9 @@
-TODO: Write Mongo Setup Script
+docker run -d \
+  --name mongodb \
+  -e MONGO_INITDB_DATABASE=ai \
+  -e MONGO_INITDB_ROOT_USERNAME=ai \
+  -e MONGO_INITDB_ROOT_PASSWORD=ai \
+  -v mongo_data:/data/db \
+  -v mongo_config:/data/configdb \
+  -p 27017:27017 \
+  mongo

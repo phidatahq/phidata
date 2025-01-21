e4ede33c757606b59b9f25ebea7a8a54b96ee071
Willem Carel de Jongh <willemcarel@gmail.com>
2025-01-21 16:38:41 +0200

    PHI-2422 Update S3 buckets

--- cookbook/agent_concepts/tools/duckdb.py
+++ cookbook/agent_concepts/tools/duckdb.py
@@ -4,7 +4,7 @@ from agno.tools.duckdb import DuckDbTools
 agent = Agent(
     tools=[DuckDbTools()],
     show_tool_calls=True,
-    system_prompt="Use this file for Movies data: https://phidata-public.s3.amazonaws.com/demo_data/IMDB-Movie-Data.csv",
+    system_prompt="Use this file for Movies data: https://agno-public.s3.amazonaws.com/demo_data/IMDB-Movie-Data.csv",
 )
 agent.print_response(
     "What is the average rating of movies?", markdown=True, stream=False

ff91a6cc9fda787ec74c076fcfac3547177d53c1
Ashpreet <ashpreetbedi@users.noreply.github.com>
2025-01-22 22:57:11 +0000

    README

--- cookbook/agent_concepts/tools/file_tools.py
+++ cookbook/agent_concepts/tools/file_tools.py
@@ -1,10 +1,9 @@
-from agno.agent import Agent
-from agno.tools.file import FileTools
 from pathlib import Path
 
-agent = Agent(tools=[FileTools(
-    Path('tmp/file')
-)], show_tool_calls=True)
+from agno.agent import Agent
+from agno.tools.file import FileTools
+
+agent = Agent(tools=[FileTools(Path("tmp/file"))], show_tool_calls=True)
 agent.print_response(
     "What is the most advanced LLM currently? Save the answer to a file.", markdown=True
 )

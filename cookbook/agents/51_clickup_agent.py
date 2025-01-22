b4a3124d67a31405ab14dfe133649bcddb195d9b
Tapan Patro <tapankumarpatro05@gmail.com>
2025-01-22 23:03:50 +0530

    Update cookbook/agents/51_clickup_agent.py
    Done
    
    Co-authored-by: Dirk Brand <51947788+dirkbrnd@users.noreply.github.com>

--- cookbook/agents/51_clickup_agent.py
+++ cookbook/agents/51_clickup_agent.py
@@ -6,7 +6,7 @@ Step 2: Navigate to Settings (usually a circle with your initials) click on it
 Step 3: Access the Apps Section: In the settings sidebar on the left, scroll down until you find Apps. Click on it to access the API settings.
 Step 4: Generate Your API Key
 In the Apps section, you should see an option labeled API Token. If it’s not already generated, look for a button that says Generate and click it.
-Once generated, your API key will be displayed. Make sure to copy this key and store it .env file to use it.
+Once generated, your API key will be displayed. Make sure to copy this key and store it as CLICKUP_API_KEY in .env file to use it.
 
 Steps To find your MASTER_SPACE_ID :
 clickup space url structure: https://app.clickup.com/{MASTER_SPACE_ID}/v/o/s/{SPACE_ID}

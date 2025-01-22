6f0baf564a826907440a65472ec8c29a529f6866
Willem Carel de Jongh <willemcarel@gmail.com>
2025-01-22 13:04:45 +0200

    refactor/small-cookbook-updates

--- cookbook/agent_concepts/other/intermediate_steps.py
+++ cookbook/agent_concepts/other/intermediate_steps.py
@@ -16,5 +16,5 @@ run_stream: Iterator[RunResponse] = agent.run(
     "What is the stock price of NVDA", stream=True, stream_intermediate_steps=True
 )
 for chunk in run_stream:
-    pprint(chunk.model_dump(exclude={"messages"}))
+    pprint(chunk.to_dict())
     print("---" * 20)

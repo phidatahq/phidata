06e5a1d3ae46e4ab8696771a52cece1423e5a378
srexrg <sreeragp276@gmail.com>
2025-01-23 00:17:11 +0530

    typo fix

--- cookbook/tools/github_tools.py
+++ cookbook/tools/github_tools.py
@@ -21,7 +21,7 @@ agent.print_response("List open pull requests", markdown=True)
 # agent.print_response("Explain the comments for the most recent issue", markdown=True)
 # # Example usage: Create a Repo
 # agent.print_response("Create a repo called phi-test and add description hello and set it to private", markdown=True)
-# # Example usage: List repositories
+# # Example usage: List Branches
 # agent.print_response("List branches of phidatahq/phidata", markdown=True)
 # # Example usage: Delete a repo
 # agent.print_response("Delete the repo called owner/repo-name", markdown=True)

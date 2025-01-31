2c5dbbea7d2c05c21735e51555fa125ea0b2d24a
Dirk Brand <dirkbrnd@gmail.com>
2025-01-31 15:19:16 +0200

    Add gmail toolkit

---
+++ cookbook/tools/gmail_tools.py
@@ -0,0 +1,29 @@
+"""
+Gmail Agent that can read, draft and send emails using the Gmail.
+"""
+from agno.agent import Agent
+from agno.tools.gmail import GmailTools
+from agno.models.google import Gemini
+
+agent = Agent(
+    name="Gmail Agent",
+    model=Gemini(id="gemini-2.0-flash-exp"),
+    tools=[
+        GmailTools()
+    ],
+    description="You are an expert Gmail Agent that can read, draft and send emails using the Gmail.",
+    instructions=[
+        "Based on user query, you can read, draft and send emails using the Gmail.",
+        "While showing email contents, you can summarize the email contents, extract key details and dates.",
+        "Show the email contents in a structured markdown format.",
+    ],
+    markdown=True,
+    show_tool_calls=False,
+    debug_mode=True,
+)
+
+agent.print_response(
+    "summarize my last 5 emails with dates and key details, regarding ai agents",
+    markdown=True,
+    stream=True
+)

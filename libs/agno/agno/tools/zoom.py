9c3cfe13a87a6970808324bd64f5995822b4a08b
Dirk Brand <dirkbrnd@gmail.com>
2025-02-03 12:40:11 +0200

    Update

--- libs/agno/agno/tools/zoom.py
+++ libs/agno/agno/tools/zoom.py
@@ -16,7 +16,6 @@ class ZoomTools(Toolkit):
         account_id: Optional[str] = None,
         client_id: Optional[str] = None,
         client_secret: Optional[str] = None,
-        name: str = "zoom_tool",
     ):
         """
         Initialize the ZoomTool.
@@ -27,7 +26,7 @@ class ZoomTools(Toolkit):
             client_secret (str): The client secret for authentication. If not provided, will use ZOOM_CLIENT_SECRET env var.
             name (str): The name of the tool. Defaults to "zoom_tool".
         """
-        super().__init__(name)
+        super().__init__("zoom_tool")
 
         # Get credentials from env vars if not provided
         self.account_id = account_id or getenv("ZOOM_ACCOUNT_ID")

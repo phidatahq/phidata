997654eb94a3bf7e9ba073a85d305274bc68bf60
ysolanky <yash@phidata.com>
2025-02-04 08:50:18 -0500

    update

--- cookbook/tools/googlesheets_tools.py
+++ cookbook/tools/googlesheets_tools.py
@@ -7,6 +7,9 @@ Setup:
 2: Save the credentials.json file to the root of the project or update the path in the GoogleSheetsTools class
 3: Run the script to authenticate and generate the token.json file. In the initial run, the script will open a browser window to authenticate the application.
 4: Update the SCOPES as per the requirements of the application.
+
+# Example spreadsheet: https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/
+The ID is the URL of the spreadsheet and the range is the sheet name and the range of cells to read.
 """
 
 

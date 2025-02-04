a07f1d86cead3a1f0f2585fc2ef30880535a7122
Vargha <vkhalokhi@gmail.com>
2025-02-04 13:27:41 +0330

    weaviate vectordb cookbook

---
+++ cookbook/agent_concepts/knowledge/vector_dbs/weaviate.py
@@ -0,0 +1,24 @@
+# install weaviate - `pip install weaviate-client`
+
+from phi.agent import Agent
+from phi.knowledge.text import TextKnowledgeBase
+from phi.vectordb.weaviate import Weaviate
+
+# run weaviate client locally
+"""
+- Run the docker image: docker pull semitechnologies/weaviate:latest
+- Then, run the service:
+   docker run -d -p 8080:8080 \
+  -v ./data:/var/lib/weaviate \
+  semitechnologies/weaviate:latest
+"""
+
+vector_db = Weaviate(index_name="weaviate_collection")
+# Create knowledge base
+knowledge_base = TextKnowledgeBase(path="data/txt_files", vector_db=vector_db)
+
+knowledge_base.load(recreate=False)  # Comment out after first run
+
+# Create and use the agent
+agent = Agent(knowledge_base=knowledge_base, search_knowledge=True, use_tools=True, show_tool_calls=True)
+agent.print_response("How to make Thai curry?", markdown=True)

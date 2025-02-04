6a34238faa19b69d46ee3542e1d39b9fcc9d2b1a
ysolanky <yash@phidata.com>
2025-02-03 23:00:27 -0500

    update

--- cookbook/tools/googlesheets_tools.py
+++ cookbook/tools/googlesheets_tools.py
@@ -1,3 +1,15 @@
+"""
+This script demonstrates how to use the GoogleSheetsTools class to interact with Google Sheets through an Agent.
+
+Setup:
+
+1: Follow the steps: https://developers.google.com/sheets/api/quickstart/python
+2: Save the credentials.json file to the root of the project or update the path in the GoogleSheetsTools class
+3: Run the script to authenticate and generate the token.json file. In the initial run, the script will open a browser window to authenticate the application.
+4: Update the SCOPES as per the requirements of the application.
+"""
+
+
 from agno.agent import Agent
 from agno.tools.googlesheets import GoogleSheetsTools
 

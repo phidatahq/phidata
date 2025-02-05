c2b21933833faf2375d2c563c0aba8e855a9a8e0
ansub <khanansub3@gmail.com>
2025-02-05 21:26:29 +0400

    Add documentation for Todoist tool usage and requirements

--- cookbook/tools/todoist_tool.py
+++ cookbook/tools/todoist_tool.py
@@ -2,6 +2,21 @@ from agno.agent import Agent
 from agno.models.openai import OpenAIChat
 from agno.tools.todoist import TodoistTools
 
+"""
+Example showing how to use the Todoist Tools with Agno
+
+Requirements:
+- Todoist API Token (get from https://app.todoist.com/app/settings/integrations/developer)
+- pip install todoist-api-python
+
+Usage:
+- Set the following environment variables:
+    export TODOIST_API_TOKEN="your_api_token"
+
+- Or provide them when creating the TodoistTools instance
+"""
+
+
 todoist_agent = Agent(
     name="Todoist Agent",
     role="Manage your todoist tasks",
